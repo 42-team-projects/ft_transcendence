27465b73c70516fd9f9805c27c3d57e36ad1ee4b
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-05-22 02:56:09 +0100

    search bar

--- main.js
+++ main.js
@@ -1,36 +1,18 @@
 const header_element = document.querySelector('header')
 
-header_object = [
-    {
-        tag : 'img',
-        id : 'pingpong-logo',
-        src : './images/pingpong-icon.svg'
-    },
-    // {
-    //     type : '',
-    //     id : 'pingpong-logo',
-    //     src : './images/pingpong-icon.svg'
-    // },
-    // {
-    //     type : 'img',
-    //     id : 'pingpong-logo',
-    //     src : './images/pingpong-icon.svg'
-    // },
-    // {
-    //     type : 'img',
-    //     id : 'pingpong-logo',
-    //     src : './images/pingpong-icon.svg'
-    // },
-];
 
 function Header()
 {
-    header_object.forEach(element => {
-        let tag = document.createElement(`${element.tag}`);
-        tag.src = element.src;
-        tag.id = element.id;
-        header_element.appendChild(tag);
-    });
+    fetch('header-bar.html')
+    .then((respons)=>{
+        if(respons.ok)
+            return respons.text()
+        else
+            console.log('error')
+    }).then((text) => {
+        console.log(text)
+        header_element.innerHTML = text
+    })
 }
 
 
