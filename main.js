168c175a2729638a12c09056019937c134137f75
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-05-26 01:56:04 +0100

    start building sidebar

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const header_element = document.querySelector('header')
+const sidebar_element = document.querySelector('.side-bar')
 
 
 function Header()
@@ -10,9 +11,30 @@ function Header()
         else
             console.log('error')
     }).then((text) => {
-        console.log(text)
+        // console.log(text)
         header_element.innerHTML = text
     })
 }
 
+
+function sideBar()
+{
+    fetch('side-bar.html')
+    .then((respons)=>{
+        if(respons.ok)
+        return respons.text()
+    else
+    console.log('error')
+}).then((text) => {
+    // console.log(text)
+    sidebar_element.innerHTML = text
+})
+}
+
+sidebar_element.addEventListener('click', ()=>{
+    console.log('hiiii')
+})
+
+
 Header()
+sideBar()
