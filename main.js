55a0b064aad3dc61efbfb26b6b7d1d202f5e697e
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-05-27 14:59:11 +0100

    use component using js

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 const header_element = document.querySelector('header')
 const sidebar_element = document.querySelector('.side-bar')
 
+const sidebar_container = [
+    {
+        img : '<img draggable="false" src="./images/Home.svg" alt="Home">',
+        content : 'HOME' 
+    },
+    {
+        img : '<img draggable="false" src="./images/Game.svg" alt="Game">',
+        content : 'GAME' 
+    },
+    {
+        img : '<img draggable="false" src="./images/Chat.svg" alt="Chat">',
+        content : 'CHAT' 
+    },
+    {
+        img : '<img draggable="false" src="./images/friends.svg" alt="Friends">',
+        content : 'FRIENDS' 
+    },
+    {
+        img : '<img draggable="false" src="./images/leaderboard.svg" alt="Leaderboard">',
+        content : 'LEADERBOARD' 
+    },
+    {
+        img : '<img draggable="false" src="./images/settings.svg" alt="Settings"></img>',
+        content : 'SETTINGS' 
+    }
+]
 
 function Header()
 {
@@ -11,7 +37,6 @@ function Header()
         else
             console.log('error')
     }).then((text) => {
-        // console.log(text)
         header_element.innerHTML = text
     })
 }
@@ -25,9 +50,15 @@ function sideBar()
         return respons.text()
     else
     console.log('error')
-}).then((text) => {
-    // console.log(text)
-    sidebar_element.innerHTML = text
+}).then((html) => {
+    sidebar_element.innerHTML = html;
+    const component = sidebar_element.querySelector('.items')
+    sidebar_element.innerHTML = ``
+    sidebar_container.forEach((element) =>{
+        let item = component;
+        console.log(item);
+        item.q
+    })
 })
 }
 
