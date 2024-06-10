a04a3d5443a4c7bd415047a20fe9fed94b87e6f4
Nourdine Ouakhroun <nouakhro@c1r2p6.1337.ma>
2024-06-10 22:57:39 +0100

    sarting building side bar using component

--- side-bar/sb-button.js
+++ side-bar/sb-button.js
@@ -4,19 +4,15 @@ const SB_ButtonTemplate = document.createElement('template')
 SB_ButtonTemplate.innerHTML = /*html*/`
     <div class="trasparentItem">
         <c-hexagon width="100%" height="100%" apply="true">
-            <style>
-                .images
-                {
-                    position: absolute;
-                    width: 50%;
-                    height: 50%;
-                    z-index: 2;
-                    transform: rotate(90deg);
-                }
-            </style>
-            <img draggable="false" src="./images/Home.svg" alt="Home" class="images">
+            <img slot="content" draggable="false" src="./images/Home.svg" alt="Home" class="images">
         </c-hexagon>
     </div>
+    <button-text> 
+        <div slot="content" class="text-tag">
+            <h1></h1>
+        </div>
+    </button-text>
+
 `
 
 class SideBarButton extends HTMLElement{
