a45cac1b557665eabddfe45cd92cd5d80adda449
Nourdine Ouakhroun <nouakhro@c1r2p6.1337.ma>
2024-06-12 20:34:40 +0100

    making all the side bar buttons

--- side-bar/button-text.js
+++ side-bar/button-text.js
@@ -1,15 +1,29 @@
 const ButtontextElement = document.createElement('template')
 
 ButtontextElement.innerHTML = /*html*/ `
-
-    <slot name="content"> </slot>
-
+    <style>
+        .text-tag{
+            position: absolute;
+            width: 98.7%;
+            height: 96.5%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: linear-gradient(48deg, #09213af2 25%, #093967de 59%,#09213af2 92%);
+            clip-path: polygon(8% 15%, 85% 15%, 98% 50%, 85% 94%, 0.3% 94%,7.5% 50%, 4.5% 33.5%,4.4% 31.5%);
+        }
+    </style>
+    <div class="text-tag">
+        <slot name="content"></slot>
+    </div>
 `
 
-class ButtonText extends HTMLElement {
+export class ButtonText extends HTMLElement {
     constructor(){
         super();
+        const shadow = this.attachShadow({
+            mode: 'open'
+        })
+        shadow.appendChild(ButtontextElement.content.cloneNode(true))
     }
 }
-
-customElements.define('button-text',ButtonText)
--- side-bar/sb-button.js
+++ side-bar/sb-button.js
@@ -1,26 +1,80 @@
 
+import {ButtonText} from './button-text.js'
+customElements.define('button-text',ButtonText)
+
 const SB_ButtonTemplate = document.createElement('template')
 
 SB_ButtonTemplate.innerHTML = /*html*/`
+    <style>
+        .trasparentItem{
+            position: relative;
+            display: flex;
+            background: transparent;
+            /* background: red; */
+            width: 25%; 
+            aspect-ratio: 1;
+            transform: rotate(270deg);
+            justify-content: center;
+        }
+
+        .trasparentItem::before{
+            content: "";
+            width: 100%;
+            height: 100%;
+            background: rgb(255, 255, 255);
+            clip-path: polygon(24.5% 1%, 31% 1.7%, 5.6% 18.6%, 5.1% 38%, 1.5% 33%, 2% 16%);
+            position: absolute;
+            top: 12.8%;
+            left: 10.8%;
+            z-index: 2;
+            position: absolute;
+        }
+
+        .trasparentItem::after{
+            content: "";
+            width: 100%;
+            height: 100%;
+            background: rgb(255, 255, 255);
+            clip-path: polygon(24.5% 2%, 31% 2.7%, 5.6% 18.6%, 5.3% 38%, 1.5% 33%, 2% 16%);
+            position: absolute;
+            bottom: 12.1%;
+            right: 10.5%;
+            z-index: 2;
+            transform: rotate(179deg);
+            position: absolute;
+        }
+        button-text{
+            position: absolute;
+            width: 83%;
+            aspect-ratio: 8 / 2;
+            left: 18%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #00fffb;
+            clip-path: polygon(8% 15%, 85% 15%, 98% 50%, 85% 94%, 0.3% 94%,7.5% 50%, 4.5% 33.5%,4.4% 31.5%);
+            border: 1px solid red;
+        }
+
+    </style>
     <div class="trasparentItem">
         <c-hexagon width="100%" height="100%" apply="true">
-            <img slot="content" draggable="false" src="./images/Home.svg" alt="Home" class="images">
+            <slot name="image" slot="content"></slot>
         </c-hexagon>
     </div>
     <button-text> 
-        <div slot="content" class="text-tag">
-            <h1></h1>
-        </div>
+        <slot name="text" slot="content"></slot>
     </button-text>
 
 `
 
-class SideBarButton extends HTMLElement{
+export class SideBarButton extends HTMLElement{
     constructor(){
         super();
-        this.appendChild(SB_ButtonTemplate.content.cloneNode(true))
-        // console.log(this.querySelector('c-hexagon'));
+        const shadow = this.attachShadow({
+            mode: 'open'
+        })
+        shadow.appendChild(SB_ButtonTemplate.content.cloneNode(true))
     }
 }
 
-customElements.define("sb-button", SideBarButton)
