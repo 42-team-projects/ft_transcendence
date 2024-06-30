1f953de83c9dd732cd3b2d825e6074ecc8927a0a
escanour <nouridn@ouakhroun.com>
2024-06-30 18:57:47 +0100

    make all the carts

--- Game/GamePlay/MultiPlayerCart.js
+++ Game/GamePlay/MultiPlayerCart.js
@@ -5,35 +5,29 @@ const MultiPlayerTemplate = document.createElement('template')
 
 MultiPlayerTemplate.innerHTML = /*html*/ `
 	<link rel="stylesheet" href="../Game/GamePlay/MultiPlayer.css">
-	<img class="BlueCartImg" src="../images/multi-playerImg.svg" alt="">
-	<img class="BlueCart" src="../../images/multiPlayerCart.svg" alt="">
+	<img class="BlueCartImg" src="../images/BlueCart/multi-playerImg.svg" alt="">
+	<img class="BlueCart" src="../../images/BlueCart/multiPlayerCart.svg" alt="">
 	<div class="shapes2-3">
-	<div class="shapes2-3">
-	</div>
+		<div class="shapes4-5">
+			<div class="text">
+				<h1>MULTI PLAYER</h1>
+			</div>
+			<div class="M-buttonC">
+				<p>
+				Lorem ipsum dolor sit amet. Ut facere consequatur est dolore placeat rem accusamus quae est odit dolore. Id impedit molestiae vel voluptates repellendus ut perferendis libero et blanditiis dolor est dolorum molestiae. 
+				</p>
+				<div class="M-buttons">
+					<div class="M-buttons-content">
+						<h1>START</h1>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	`
 /*
 	<div class="style-1">
 		<div class="style-2">
-			<div class="style-2_1">
-				<h1>SINGLE PLAYER</h1>
-			</div>
-			<div class="box-container">
-				<div class="top-box">
-					<div class="middle-box">
-					</div>
-				</div>
-			</div>
-			<div class="buttonC">
-				<p>
-				Lorem ipsum dolor sit amet. Ut facere consequatur est dolore placeat rem accusamus quae est odit dolore. Id impedit molestiae vel voluptates repellendus ut perferendis libero et blanditiis dolor est dolorum molestiae. 
-				</p>
-				<div class="buttons">
-					<div class="buttons-content">
-						<h1>START</h1>
-					</div>
-				</div>
-			</div>
 	
 		</div>
 	</div>
