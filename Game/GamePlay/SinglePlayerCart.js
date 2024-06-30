8e39743e6f4a2974af0267597d83fe2336e7374f
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-06-30 16:06:02 +0100

    make a blue carts

--- Game/GamePlay/SinglePlayerCart.js
+++ Game/GamePlay/SinglePlayerCart.js
@@ -5,35 +5,36 @@ const singlePlayerTemplate = document.createElement('template')
 
 singlePlayerTemplate.innerHTML = /*html*/ `
     <link rel="stylesheet" href="../Game/GamePlay/Game-player.css">
+    <img class="GreenCartImg" src="../images/img-singleplayer.svg" alt="">
+    <img class="GreenCart" src="../../images/Single.svg" alt="">
+    <div class="style-0">
+        <div class="style-1">
+            <div class="style-2">
+                <div class="style-2_1">
+                    <h1>SINGLE PLAYER</h1>
+                </div>
+                <div class="box-container">
+                    <div class="top-box">
+                        <div class="middle-box">
+                        </div>
+                    </div>
+                </div>
+                <div class="buttonC">
+                    <p>
+                    Lorem ipsum dolor sit amet. Ut facere consequatur est dolore placeat rem accusamus quae est odit dolore. Id impedit molestiae vel voluptates repellendus ut perferendis libero et blanditiis dolor est dolorum molestiae. 
+                    </p>
+                    <div class="buttons">
+                        <div class="buttons-content">
+                            <h1>START</h1>
+                        </div>
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+    </div>
     `
 /*
-    <div class="style-0">
-    </div>
-<div class="style-1">
-    <div class="style-2">
-        <img class="GreenCartImg" src="./images/img-singleplayer.svg" alt="">
-        <div class="style-2_1">
-            <h1>SINGLE PLAYER</h1>
-        </div>
-        <div class="box-container">
-        <div class="top-box">
-            <div class="middle-box">
-            </div>
-        </div>
-        </div>
-        <div class="style-3">
-            <img src="" alt="">
-        </div>
-        <div class="buttonC">
-            <p>
-            Lorem ipsum dolor sit amet. Ut facere consequatur est dolore placeat rem accusamus quae est odit dolore. Id impedit molestiae vel voluptates repellendus ut perferendis libero et blanditiis dolor est dolorum molestiae. 
-            </p>
-            <div class="buttons">
-                <div class="buttons-content">
-                    <h1>START</h1>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
             */
