fcc0b65c18b8f6dbe7c5d8d71b0e16d46a4ae9cf
escanour <nouridn@ouakhroun.com>
2024-07-01 04:54:05 +0100

    make Ai lobby

--- Game/GamePlay/GameOnline/OnlineGameCart.js
+++ Game/GamePlay/GameOnline/OnlineGameCart.js
@@ -1,11 +1,12 @@
 
+import {Buttons} from '../buttons.js'
 
 const OnlineGameTemplate = document.createElement('template')
 
 
 OnlineGameTemplate.innerHTML = /*html*/ `
-	<link rel="stylesheet" href="../Game/GamePlay/OnlineGame.css">
-	<img class="OrangeCartImg" src="../images/OrangeCart/OrangeCartImg.svg" alt="">
+	<link rel="stylesheet" href="../../Game/GamePlay/GameOnline/OnlineGame.css">
+	<img class="OrangeCartImg" src="../../images/OrangeCart/OrangeCartImg.svg" alt="">
 	<div class="shapes3-4">
 		<div class="Online-text">
 			<h1>ONLINE GAME</h1>
@@ -14,26 +15,12 @@ OnlineGameTemplate.innerHTML = /*html*/ `
 			<p>
 			Lorem ipsum dolor sit amet. Ut facere consequatur est dolore placeat rem accusamus quae est odit dolore. Id impedit molestiae vel voluptates repellendus ut perferendis libero et blanditiis dolor est dolorum molestiae. 
 			</p>
-			<div class="O-buttons">
-				<div class="O-buttons-content">
-					<h1>START</h1>
-				</div>
-			</div>
+			<c-button Bcolor="#EB9A45" Hcolor="#e98f2f"> 
+				<h1 slot="text">START</h1>
+			</c-button>
 		</div>
 	</div>
 	`
-/*
-		</div>
-	</div>
-	<div class="style-1">
-		<div class="style-2">
-	
-		</div>
-	</div>
-</div>
-</div>
-			*/
-						// <div class="mask1"></div>
 
 export class OnlineGame extends HTMLElement{
 
