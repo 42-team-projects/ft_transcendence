09a2dfdd22c57d0ab56286c44f8a1197224e5c49
ogorfti <ww.shanks1@gmail.com>
2024-07-03 14:53:42 +0100

    jwt done

---
+++ backend/accounts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils.translation import gettext as _
+from django.utils import timezone
+from .managers import UserManager
+from datetime import timedelta
+from uuid import uuid4
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True, verbose_name=_('Username'))
+    email = models.EmailField(max_length=255, unique=True, verbose_name=_('Email Address'))
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.username
+
+    @property
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        print(refresh)
+        tokens = {
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh),
+        }
+        return tokens
+
+class OTP(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user.username}--otpcode"
---
+++ backend/accounts/views.py
@@ -0,0 +1,60 @@
+from rest_framework import generics
+from .serializers import UserRegisterSerializer, LoginSerializer, ConfirmEmailSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .utils import send_otp, send_confirmation_email, custom_token_generator
+from .models import OTP, User
+# from datetime import timedelta, datetime
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from rest_framework.permissions import IsAuthenticated
+
+
+class UserRegisterView(generics.GenericAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request):
+        serializer=self.serializer_class(data=request.data)
+        if (serializer.is_valid(raise_exception=True)):
+            user = serializer.save()
+            token = custom_token_generator.make_token(user)
+            # print(f"sending token: {token}")
+            # send_otp(request)
+            send_confirmation_email(user, request, token)
+            return Response({
+                'message': f"Thank you for registering, {user.username}.",
+                'data': UserRegisterSerializer(user).data,
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyOTPView(generics.GenericAPIView):
+    def post(self, request):
+        otp_code = request.data.get('otp')
+        try:
+            otp = OTP.objects.get(otp=otp_code)
+            user = otp.user
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response({'status': 'success', 'message': 'OTP verified successfully'}, status=status.HTTP_200_OK)
+            return Response({'status': 'error', 'message': 'User is already verified'}, status=status.HTTP_400_BAD_REQUEST)
+        except OTP.DoesNotExist:
+            return Response({'status': 'error', 'message': 'OTP does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginUserView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ConfirmEmailView(generics.GenericAPIView):
+    serializer_class = ConfirmEmailSerializer
+
+    def get(self, request, uidb64, token):
+        serializer = self.get_serializer(data={'uidb64': uidb64, 'token': token})
+        serializer.is_valid(raise_exception=True)
+        return Response({'status': 'success', 'message': 'Email confirmed successfully'}, status=status.HTTP_200_OK)
