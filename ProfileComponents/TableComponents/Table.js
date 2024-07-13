92970a3cda6a64ea71262519af43eff046594e6c
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-13 22:46:25 +0100

    beffor mergin with nourdine

--- ProfileComponents/TableComponents/Table.js
+++ ProfileComponents/TableComponents/Table.js
@@ -2,48 +2,48 @@ import { DateComponent } from "./BodyComponents/Date/DateComponent.js";
 
 const url = "https://jsonplaceholder.org/users";
 
-const fakeData = [
-    {
-        id: 1,
-        date: {day: 19, month: "MAY"},
-        username: "ESALIM",
-        profile: "https://google.com/search",
-        time: "2 HOURS AGO",
-        score: "5 - 6",
-        result: "WIN",
-        replay: ">"
-    },
-    {
-        id: 1,
-        date: {day: 19, month: "MAY"},
-        username: "ESALIM",
-        profile: "https://google.com/search",
-        time: "2 HOURS AGO",
-        score: "5 - 6",
-        result: "WIN",
-        replay: ">"
-    },
-    {
-        id: 1,
-        date: {day: 19, month: "MAY"},
-        username: "ESALIM",
-        profile: "https://google.com/search",
-        time: "2 HOURS AGO",
-        score: "5 - 6",
-        result: "WIN",
-        replay: ">"
-    },
-    {
-        id: 1,
-        date: {day: 19, month: "MAY"},
-        username: "ESALIM",
-        profile: "https://google.com/search",
-        time: "2 HOURS AGO",
-        score: "5 - 6",
-        result: "WIN",
-        replay: ">"
-    }
-];
+// const fakeData = [
+//     {
+//         id: 1,
+//         date: {day: 19, month: "MAY"},
+//         username: "ESALIM",
+//         profile: "https://google.com/search",
+//         time: "2 HOURS AGO",
+//         score: "5 - 6",
+//         result: "WIN",
+//         replay: ">"
+//     },
+//     {
+//         id: 1,
+//         date: {day: 19, month: "MAY"},
+//         username: "ESALIM",
+//         profile: "https://google.com/search",
+//         time: "2 HOURS AGO",
+//         score: "5 - 6",
+//         result: "WIN",
+//         replay: ">"
+//     },
+//     {
+//         id: 1,
+//         date: {day: 19, month: "MAY"},
+//         username: "ESALIM",
+//         profile: "https://google.com/search",
+//         time: "2 HOURS AGO",
+//         score: "5 - 6",
+//         result: "WIN",
+//         replay: ">"
+//     },
+//     {
+//         id: 1,
+//         date: {day: 19, month: "MAY"},
+//         username: "ESALIM",
+//         profile: "https://google.com/search",
+//         time: "2 HOURS AGO",
+//         score: "5 - 6",
+//         result: "WIN",
+//         replay: ">"
+//     }
+// ];
 
 export class Table extends HTMLElement {
     
@@ -63,8 +63,7 @@ export class Table extends HTMLElement {
         } catch (error) {
             console.error(error.message);
         }
-        */
-       return fakeData;
+    //    return fakeData;
     }
 
     getTableHeader(titles) {
@@ -78,32 +77,45 @@ export class Table extends HTMLElement {
         tableHeader.appendChild(tableRow);
         return tableHeader;
     }
-   
+   /*
+       <c-hexagon width="110px" height="110px" apply="true" >
+        <img slot="content" draggable="false" src="./images/svg-header/profile.jpeg">
+    </c-hexagon>
+   */
     getTableBody(data) {
         const tableBody = document.createElement("tbody");
         const keys = Object.keys(data[0]).splice(1);
-        let counter = 0;
         data.forEach(element => {
             const tableRow = document.createElement("tr");
             const values = Object.values(element).splice(1);
-            let isFirst = true;
-            counter++;
+            let counter = 0;
             values.forEach( val => {
                 const tableData = document.createElement("td");
-                if (isFirst) {
+                if (counter == 0) {
                     const dateComponent = document.createElement("date-component");
                     dateComponent.day = val.day;
                     dateComponent.month = val.month;
                     tableData.appendChild(dateComponent);
                 } else {
-                    
                     const parag = document.createElement("p");
                     parag.className = "table-time";
                     parag.textContent = val;
                     parag.textContent = parag.textContent.toUpperCase();
                     tableData.appendChild(parag);
+                // } else if (counter == 2)  {
+                //     const profile = document.createElement("c-hexagon");
+                //     profile.width = "50px";
+                //     profile.height = "50px";
+                //     profile.apply = true;
+                //     profile.bcolor = "#EB9A45";
+                //     const profileImage = document.createElement("img");
+                //     profileImage.slot = "content";
+                //     profileImage.draggable = "false";
+                //     profileImage.src = "/images/svg-header/profile.jpeg";
+                //     profile.appendChild(profileImage);
+                //     tableData.appendChild(profile);
                 }
-                isFirst = false;
+                counter++;
                 tableRow.appendChild(tableData);
             });
             tableBody.appendChild(tableRow);
