885e6324ab60c86cd2b594c7ea6352a5f1439f5e
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-13 21:05:36 +0100

    add table component.

---
+++ ProfileComponents/app.js
@@ -0,0 +1,5 @@
+import { DateComponent } from "./TableComponents/BodyComponents/Date/DateComponent.js";
+import { Table } from "./TableComponents/Table.js";
+
+customElements.define("date-component", DateComponent);
+window.customElements.define("my-table", Table);
