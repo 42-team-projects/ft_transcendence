5170e1f5c3f7ac11cd9e2836aa190876a450e640
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-13 23:08:50 +0100

    update profile image.

--- ProfileComponents/TableComponents/Table.js
+++ ProfileComponents/TableComponents/Table.js
@@ -2,48 +2,48 @@ import { DateComponent } from "./BodyComponents/Date/DateComponent.js";
 
 const url = "https://jsonplaceholder.org/users";
 
-// const fakeData = [
-//     {
-//         id: 1,
-//         date: {day: 19, month: "MAY"},
-//         username: "ESALIM",
-//         profile: "https://google.com/search",
-//         time: "2 HOURS AGO",
-//         score: "5 - 6",
-//         result: "WIN",
-//         replay: ">"
-//     },
-//     {
-//         id: 1,
-//         date: {day: 19, month: "MAY"},
-//         username: "ESALIM",
-//         profile: "https://google.com/search",
-//         time: "2 HOURS AGO",
-//         score: "5 - 6",
-//         result: "WIN",
-//         replay: ">"
-//     },
-//     {
-//         id: 1,
-//         date: {day: 19, month: "MAY"},
-//         username: "ESALIM",
-//         profile: "https://google.com/search",
-//         time: "2 HOURS AGO",
-//         score: "5 - 6",
-//         result: "WIN",
-//         replay: ">"
-//     },
-//     {
-//         id: 1,
-//         date: {day: 19, month: "MAY"},
-//         username: "ESALIM",
-//         profile: "https://google.com/search",
-//         time: "2 HOURS AGO",
-//         score: "5 - 6",
-//         result: "WIN",
-//         replay: ">"
-//     }
-// ];
+const fakeData = [
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    },
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    },
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    },
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    }
+];
 
 export class Table extends HTMLElement {
     
@@ -63,7 +63,8 @@ export class Table extends HTMLElement {
         } catch (error) {
             console.error(error.message);
         }
-    //    return fakeData;
+        */
+       return fakeData;
     }
 
     getTableHeader(titles) {
@@ -96,24 +97,26 @@ export class Table extends HTMLElement {
                     dateComponent.day = val.day;
                     dateComponent.month = val.month;
                     tableData.appendChild(dateComponent);
-                } else {
+                } else if (counter == 1) {
                     const parag = document.createElement("p");
                     parag.className = "table-time";
                     parag.textContent = val;
                     parag.textContent = parag.textContent.toUpperCase();
                     tableData.appendChild(parag);
-                // } else if (counter == 2)  {
-                //     const profile = document.createElement("c-hexagon");
-                //     profile.width = "50px";
-                //     profile.height = "50px";
-                //     profile.apply = true;
-                //     profile.bcolor = "#EB9A45";
-                //     const profileImage = document.createElement("img");
-                //     profileImage.slot = "content";
-                //     profileImage.draggable = "false";
-                //     profileImage.src = "/images/svg-header/profile.jpeg";
-                //     profile.appendChild(profileImage);
-                //     tableData.appendChild(profile);
+                } else if (counter == 2)  {
+                    const profile = document.createElement("c-hexagon");
+                    profile.width = "50px";
+                    profile.height = "50px";
+                    profile.apply = true;
+                    profile.Bcolor = "#EB9A45";
+                    const profileImage = document.createElement("img");
+                    profileImage.slot = "content";
+                    profileImage.width = 50;
+                    profileImage.width = 50;
+                    profileImage.draggable = "false";
+                    profileImage.src = "/images/svg-header/profile.jpeg";
+                    profile.appendChild(profileImage);
+                    tableData.appendChild(profile);
                 }
                 counter++;
                 tableRow.appendChild(tableData);
