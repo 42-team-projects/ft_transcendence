07c3aa6fe50f82ccd1f70bb14392b7e9866b0179
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-13 22:01:12 +0100

    fetching json data from jsonplaceholder.com to test table component.

--- ProfileComponents/TableComponents/Table.js
+++ ProfileComponents/TableComponents/Table.js
@@ -1,48 +1,49 @@
 import { DateComponent } from "./BodyComponents/Date/DateComponent.js";
 
-const htmlContent = `
-<div class="profile-data-stats-history">
-    <table cellspacing="0" cellpadding="0">
-        <thead>
-            <tr>
-                <th>DATE</th>
-                <th>USER NAME</th>
-                <th>PROFILE</th>
-                <th>TIME</th>
-                <th>SCORE</th>
-                <th>RESUALT</th>
-                <th>REPLAY</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="table-date">
-                    <p class="table-date-day">19</p>
-                    <p class="table-date-month">MAY</p>
-                </td>
-                <td class="table-username">
-                    <p>ESALIM</p>
-                </td>
-                <td class="table-profile">
-                    <img src="assets/profile-assets/profile-image.svg" width="40" />
-                </td>
-                <td class="table-time">
-                    <p>2 HOURS AGO</p>
-                </td>
-                <td class="table-score">
-                    <p>2 - 5</p>
-                </td>
-                <td class="table-result">
-                    <p>LOSS</p>
-                </td>
-                <td class="table-replay">
-                    <img src="assets/profile-assets/play-button.svg" width="32" />
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-`;
+const url = "https://jsonplaceholder.org/users";
+
+const fakeData = [
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    },
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    },
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    },
+    {
+        id: 1,
+        date: {day: 19, month: "MAY"},
+        username: "ESALIM",
+        profile: "https://google.com/search",
+        time: "2 HOURS AGO",
+        score: "5 - 6",
+        result: "WIN",
+        replay: ">"
+    }
+];
 
 export class Table extends HTMLElement {
     
@@ -50,10 +51,10 @@ export class Table extends HTMLElement {
         super();
     }
 
-    async getDataFromApi() {
-        const url = "https://jsonplaceholder.org/users";
+    async getDataFromApi(APIUrl) {
+        /*
         try {
-            const response = await fetch(url);
+            const response = await fetch(APIUrl);
             if (!response.ok) {
               throw new Error(`Response status: ${response.status}`);
             }
@@ -62,6 +63,8 @@ export class Table extends HTMLElement {
         } catch (error) {
             console.error(error.message);
         }
+        */
+       return fakeData;
     }
 
     getTableHeader(titles) {
@@ -78,19 +81,29 @@ export class Table extends HTMLElement {
    
     getTableBody(data) {
         const tableBody = document.createElement("tbody");
-        const keys = Object.keys(data[0]).splice(1, 7);
+        const keys = Object.keys(data[0]).splice(1);
+        let counter = 0;
         data.forEach(element => {
             const tableRow = document.createElement("tr");
-            const values = Object.values(element).splice(1, 7);
+            const values = Object.values(element).splice(1);
+            let isFirst = true;
+            counter++;
             values.forEach( val => {
                 const tableData = document.createElement("td");
-                // const parag = document.createElement("p");
-                // parag.className = "table-time";
-                // parag.textContent = val;
-                const dateComponent = document.createElement("date-component");
-                dateComponent.day = 19;
-                dateComponent.month = val;
-                tableData.appendChild(dateComponent);
+                if (isFirst) {
+                    const dateComponent = document.createElement("date-component");
+                    dateComponent.day = val.day;
+                    dateComponent.month = val.month;
+                    tableData.appendChild(dateComponent);
+                } else {
+                    
+                    const parag = document.createElement("p");
+                    parag.className = "table-time";
+                    parag.textContent = val;
+                    parag.textContent = parag.textContent.toUpperCase();
+                    tableData.appendChild(parag);
+                }
+                isFirst = false;
                 tableRow.appendChild(tableData);
             });
             tableBody.appendChild(tableRow);
@@ -113,9 +126,9 @@ export class Table extends HTMLElement {
         const table = document.createElement("table");
         table.cellSpacing = 0;
         table.cellPadding = 0;
-        const data = this.getDataFromApi();
+        const data = this.getDataFromApi(url);
         const json = data.then((values) => {
-            table.appendChild(this.getTableHeader(Object.keys(values[0]).splice(1, 7)));
+            table.appendChild(this.getTableHeader(Object.keys(values[0]).splice(1)));
             table.appendChild(this.getTableBody(values));
         });
         tableContainer.appendChild(table);
