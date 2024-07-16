decf7d131ce8a8ca9bbcf68d62114fb8f73ebd5b
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-16 14:46:42 +0100

    add 'add-friend' button to profile.

--- frontend/ProfileComponents/ProfileComponent.js
+++ frontend/ProfileComponents/ProfileComponent.js
@@ -3,19 +3,78 @@ const fakeData = {
     userName: "Esalim",
     joinDate: "27 JUL 2058",
     active: true,
+    isFriend: false,
     profileImage: "./images/svg-header/profile.jpeg",
     cover: "/frontend/images/xxxxxx.png",
-    league: "gold",
-    rank: 200,
-    nextLeague: "paltinum",
-    fullName: "mehdi salim",
-    email: "mehdi@salim.com",
-    phone: "+212612345678",
-    gender: "Male",
-    birthday: "23 JUL 2087",
-    win: 89,
-    loss: 11
+    stats: {
+        win: 89,
+        loss: 11,
+        rank: 200,
+        league: "gold",
+        nextLeague: "paltinum",
+        progressBar: 89,
+        graph: [
+            {
+                label: "win",
+                value: 89
+            },
+            {
+                label: "loss",
+                value: 11
+            },
+            {
+                label: "draw",
+                value: 5
+            },
+            {
+                label: "max-score",
+                value: 90
+            }
+        ]
+    },
+    links: [
+        {
+            title: "Google",
+            url: "https://www.google.com/search"
+        },
+        {
+            title: "youtube",
+            url: "https://www.youtube.com/search"
+        },
+        {
+            title: "Facebook",
+            url: "https://www.Facebook.com/search"
+        },
+        {
+            title: "Instagram",
+            url: "https://www.Instagram.com/search"
+        }
+    ],
+    achievements: [
+        {
+            name: "Bronze",
+            img: "/frontend/assets/profile-assets/GOLD-LEAGUE.svg"
+        },
+        {
+            name: "Silver",
+            img: "/frontend/assets/profile-assets/GOLD-LEAGUE.svg"
+        },
+        {
+            name: "Gold",
+            img: "/frontend/assets/profile-assets/GOLD-LEAGUE.svg"
+        },
+        {
+            name: "Platinum",
+            img: "/frontend/assets/profile-assets/GOLD-LEAGUE.svg"
+        }
+    ],
+    history: {
+        srcUser: 1,
+        targetUser: 78
+    }
 };
+
+const emptyGraph = [{label: "", value: 0}];
 
 export class ProfileComponent extends HTMLElement {
     constructor () {
@@ -23,8 +82,70 @@ export class ProfileComponent extends HTMLElement {
         this.attachShadow({mode: "open"});
     }
 
+    setUpLinks() {
+        const linksContainer = this.shadowRoot.getElementById("links");
+        linksContainer.innerHTML = "";
+        fakeData.links.forEach( (link) => {
+            const linkComponent = document.createElement("link-component");
+            linkComponent.link = link.url;
+            linkComponent.textContent = link.title;
+            linksContainer.appendChild(linkComponent);
+        });
+    }
+
+    setUpAchievements() {
+        const achievements = this.shadowRoot.getElementById("achievements");
+        const achievementsContainer = document.createElement("div");
+        achievementsContainer.className = "flexClass";
+        fakeData.achievements.forEach( (achievement) => {
+            const achievementComponent = document.createElement("achievement-component");
+            achievementComponent.icon = achievement.img;
+            achievementComponent.league = achievement.name;
+            achievementsContainer.appendChild(achievementComponent);
+        });
+        achievements.appendChild(achievementsContainer);
+    }
+
+    setUpGraph() {
+        const graph = this.shadowRoot.getElementById("graph");
+        graph.dataObject = (fakeData.stats.graph) ? fakeData.stats.graph : emptyGraph;
+    }
+
+    setUpUserInfo() {
+        const userInfo = this.shadowRoot.getElementById("userInfo");
+        userInfo.innerHTML = "";
+        const userInfoContainer = document.createElement("div");
+        userInfoContainer.className = "gridClass";
+        const UserKeys = ["fullName", "userName", "email", "phone", "gender", "birthday"];
+        for (let key of UserKeys) {
+            if (!fakeData[key])
+                    continue ;
+            const userInfoComponent = document.createElement("user-info-component");
+            userInfoComponent.label = key.toUpperCase();
+            userInfoComponent.value = fakeData[key];
+            userInfoContainer.appendChild(userInfoComponent);
+        }
+        userInfo.appendChild(userInfoContainer);
+
+    }
+
     connectedCallback(){
         this.shadowRoot.innerHTML = htmlContent;
+
+        this.setUpUserInfo();
+
+        this.setUpGraph();
+
+        this.setUpLinks();
+
+        this.setUpAchievements();
+
+
+    }
+
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`Attribute ${name} has changed.`);
     }
 }
 
@@ -40,35 +161,15 @@ const htmlContent = `
                 <cover-component src="${fakeData.cover}"></cover-component>
                 <div class="profile-data">
                     <div class="profile-data-infos">
-                        <profile-info-component src="${fakeData.profileImage}" username="${fakeData.userName}" joindate="${fakeData.joinDate}" league="${fakeData.league}" ${fakeData.active ? "active" : "" }></profile-info-component>
+                        <profile-info-component src="${fakeData.profileImage}" username="${fakeData.userName}" joindate="${fakeData.joinDate}" league="${fakeData.stats.league}" ${fakeData.active && "active" } ${fakeData.isFriend && "friend"}></profile-info-component>
 
                         <div class="profile-data-infos-container">
 
-                            <user-info-container label="Account Information" icon="assets/profile-assets/account-icon.svg">
-
-                                <div class="gridClass">
-                                    <user-info-component label="FULL NAME" value="${fakeData.fullName}"></user-info-component>
-                                    <user-info-component label="USERNAME" value="@${fakeData.userName}"></user-info-component>
-                                    <user-info-component label="EMAIL" value="${fakeData.email}"></user-info-component>
-                                    <user-info-component label="PHONE" value="${fakeData.phone}"></user-info-component>
-                                    <user-info-component label="GENDER" value="${fakeData.gender}"></user-info-component>
-                                    <user-info-component label="BIRTHDAY" value="${fakeData.birthday}"></user-info-component>
-                                </div>
-                            </user-info-container>
+                            <user-info-container id="userInfo" label="Account Information" icon="assets/profile-assets/account-icon.svg"></user-info-container>
                                 
-                            <user-info-container label="Links" icon="assets/profile-assets/chain-for-links.svg">
-                                <link-component link="https://www.google.com/search">Google</link-component>
-                                <link-component link="https://www.google.com/search">Facebook</link-component>
-                                <link-component link="https://www.google.com/search">Instagram</link-component>
-                            </user-info-container>
+                            <user-info-container id="links" label="Links" icon="assets/profile-assets/chain-for-links.svg"></user-info-container>
 
-                            <user-info-container label="Achievements" icon="assets/profile-assets/trophy.svg">
-                                <div class="flexClass">
-                                    <achievement-component icon="/frontend/assets/profile-assets/GOLD-LEAGUE.svg" league="bronze"></achievement-component>
-                                    <achievement-component icon="/frontend/assets/profile-assets/gold_league.svg" league="gold"></achievement-component>
-                                    <achievement-component icon="/frontend/assets/profile-assets/platinum_league.svg" league="Platinum"></achievement-component>
-                                </div>
-                            </user-info-container>
+                            <user-info-container id="achievements" label="Achievements" icon="assets/profile-assets/trophy.svg"> </user-info-container>
                             
                         </div>
                     </div>
@@ -77,23 +178,24 @@ const htmlContent = `
                         <stats-container>
                             <div class="league-bar" slot="league-bar">
                                 <league-info>
-                                    <league-item slot="current-rank" title="current rank" league="${fakeData.league}" color="#EB9A45">
-                                        <h1 class="league-rank">${fakeData.rank}</h1>
+                                    <league-item slot="current-rank" title="current rank" league="${fakeData.stats.league}" color="#EB9A45">
+                                        <h1 class="league-rank">${fakeData.stats.rank}</h1>
                                     </league-item>
                                     <div class="league-logo" slot="league-logo">
                                         <img class="current-league-logo" src="assets/profile-assets/gold_league.svg">
                                     </div>
-                                    <league-item slot="next-league" title="next league" league="${fakeData.nextLeague}" color="rgb(85, 146, 226)">
+                                    <league-item slot="next-league" title="next league" league="${fakeData.stats.nextLeague}" color="rgb(85, 146, 226)">
                                         <img class="next-league-logo" src="assets/profile-assets/platinum_league.svg">
                                     </league-item>
                                 </league-info>
                                 <custom-progress-bar></custom-progress-bar>
                             </div>
                             <div class="match-record" slot="match-record">
-                                <record-component win="${fakeData.win}" loss="${fakeData.loss}" winrate="${(100 * fakeData.win) / (fakeData.win + fakeData.loss)}" ></record-component>
+                                <record-component win="${fakeData.stats.win}" loss="${fakeData.stats.loss}" winrate="${(100 * fakeData.stats.win) / (fakeData.stats.win + fakeData.stats.loss)}" ></record-component>
                             </div>
                         </stats-container>
-                        <custom-graph></custom-graph>
+                        <custom-graph id="graph">
+                        </custom-graph>
                         <custom-table></custom-table>
                     </div>
                 </div>
