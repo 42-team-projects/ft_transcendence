2972ba04378eadcabe00dbde9364a49b02241e13
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-17 20:43:28 +0100

    create chat list and chat item components.

--- frontend/ChatComponents/ChatComponent.js
+++ frontend/ChatComponents/ChatComponent.js
@@ -7,8 +7,14 @@ export class ChatComponent extends HTMLElement {
     connectedCallback() {
         this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="/frontend/ChatComponents/ChatComponent.css">
+            
             <div class="main-container">
-                <div class="chat-list"></div>
+                <page-name width="13%">
+                    <div slot="text" class="pageNameText">
+                        <h1>Chat</h1>
+                    </div>
+                </page-name>
+                <chat-list></chat-list>
                 <div class="chat-room"></div>
                 <div class="friend-list"></div>
             </div>
