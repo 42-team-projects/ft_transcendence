a79ff277be3e228fea64955081d32ac9f7fdb776
Nourdine Ouakhroun <nouakhro@c1r2p6.1337.ma>
2024-07-19 00:26:29 +0100

    add score changing

--- frontend/Game/GamePlay/GameOnline/launchingGame.js
+++ frontend/Game/GamePlay/GameOnline/launchingGame.js
@@ -1,35 +1,4 @@
 
-
-
-{/* <div class="typing" >
-<p>Hi Players before launching the game we want to tell you the game rules</p>
-</div> */}
-// p{
-//     font-size: clamp(0.5rem, 5vw, 4rem);
-//     color: white;
-//     text-shadow: 0 0 1px #00b9be;
-// }
-// in  each 2 rounds the ball are given to the oder player in the start the ball given to player 1
-// .typing p {
-//     overflow: hidden;
-//     border-right: 0.15em solid white;
-//     white-space: nowrap;
-//     margin: 0;
-//     letter-spacing: 0.15em;
-//     animation: typing 2s steps(40, end), blink-caret 0.8s step-end infinite;
-//     font-size: 2em;
-//     color: white;
-// }
-
-// @keyframes typing {
-//     from { width: 0; }
-//     to { width: 100%; }
-// }
-
-// @keyframes blink-caret {
-//     from, to { border-color: transparent; }
-//     50% { border-color: white; }
-// }
 export class LaunchingGame extends HTMLElement{
     constructor(Time, RoundNumber)
     {
@@ -43,17 +12,15 @@ export class LaunchingGame extends HTMLElement{
                     text-shadow: 0 0 1px #00b9be;
                 }
             </style>
-            <p>Round ${RoundNumber}</p>
-            <p>${Time}</p>
+            <p id="round">Round ${RoundNumber}</p>
+            <p id="time">${Time}</p>
         `
         this.attachShadow({mode: 'open'})
         this.shadowRoot.appendChild(launching_game.content.cloneNode(true))
     }
     updateTimer(time, RoundNumber){
-        this.shadowRoot.firstElementChild.textContent = `${RoundNumber}`
-        this.shadowRoot.lastElementChild.textContent = `${time}`
+        this.shadowRoot.getElementById('round').textContent = `Round ${RoundNumber}`
+        this.shadowRoot.getElementById('time').textContent = `${time}`
     }
 }
-// <p>Round 1</p>
-// <p>${Time}</p>
 customElements.define('launching-game', LaunchingGame)
