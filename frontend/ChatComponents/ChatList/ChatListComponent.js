aba7cdc1a756023963361298825b84eda0abd994
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-24 13:26:41 +0100

    create fitcher funtion for fitching data from api.

--- frontend/ChatComponents/ChatList/ChatListComponent.js
+++ frontend/ChatComponents/ChatList/ChatListComponent.js
@@ -1,98 +1,30 @@
-const fakeAPI = [
-    {
-        id: 1,
-        userId: 1,
-        userName: "ESALIM",
-        targetUserId: 2,
-        lastMessage: "last message ...",
-        time: "12:58 PM",
-        numberOfMessage: 1
-    },
-    {
-        id: 2,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 3,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 4,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 5,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 6,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 7,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 8,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    },
-    {
-        id: 9,
-        userId: 1,
-        userName: "nouakhro",
-        targetUserId: 3,
-        lastMessage: "hello test ...",
-        time: "12:08 PM",
-        numberOfMessage: 12
-    }
-];
+import { fetchData } from "../../Utils/Fetcher.js";
+
+// let fakeData = [
+//     {
+//         id: 1,
+//         userId: 1,
+//         userName: "ESALIM",
+//         targetUserId: 2,
+//         lastMessage: "last message ...",
+//         time: "12:58 PM",
+//         numberOfMessage: 1
+//     },
+//     {
+//         id: 2,
+//         userId: 1,
+//         userName: "nouakhro",
+//         targetUserId: 3,
+//         lastMessage: "hello test ...",
+//         time: "12:08 PM",
+//         numberOfMessage: 12
+//     }
+// ];
 
 export class ChatListComponent extends HTMLElement {
     constructor () {
         super();
         this.attachShadow({mode: "open"});
-    }
-
-    selectItem;
-
-    get selectItem() { return this.selectItem; }
-
-    connectedCallback() {
         this.shadowRoot.innerHTML = `
             <style>
                 ${cssContent}
@@ -101,22 +33,38 @@ export class ChatListComponent extends HTMLElement {
             <div class="list-item" id="list-items">
             </div>
         `;
+    }
+
+    selectItem;
+
+    get selectItem() { return this.selectItem; }
+
+    createChatItem(item) {
+        const chatItem = document.createElement("chat-item");
+        chatItem.id = item.id;
+        chatItem.backgroundColor = "transparent";
+        chatItem.opacity = 0.6;
+        chatItem.userName = item.userName;
+        chatItem.lastMessage = item.lastMessage;
+        chatItem.time = item.time;
+        chatItem.numberOfMessage = item.numberOfMessage;
+        return chatItem;
+    }
+
+    connectedCallback() {
         const list = this.shadowRoot.getElementById("list-items");
+
+        const promise = fetchData("http://localhost:8080/api/v1/chat/3");
+        promise.then( data => {
+            if (data)
+            {
+                data.forEach(item => list.appendChild(this.createChatItem(item)));
+                this.eventListener();
+            }
+        });
 
 
         // chatItem.setAttribute("background-color", "#051d31");
-        fakeAPI.forEach((item) => {
-            const chatItem = document.createElement("chat-item");
-            chatItem.id = item.id;
-            chatItem.backgroundColor = "transparent";
-            chatItem.opacity = 0.6;
-            chatItem.userName = item.userName;
-            chatItem.lastMessage = item.lastMessage;
-            chatItem.time = item.time;
-            chatItem.numberOfMessage = item.numberOfMessage;
-            list.appendChild(chatItem);
-        });
-        this.eventListener();
     }
 
     eventListener() {
@@ -131,7 +79,9 @@ export class ChatListComponent extends HTMLElement {
                 }
                 item.backgroundColor = "#051d31";
                 item.opacity = 1;
+                console.log("seletor id : ", item.id);
                 this.selectItem = item.id;
+                fetchData("http://localhost:8080/api/v1/users/" + item.id).then( data => console.log(data));
             });
         });
     }
