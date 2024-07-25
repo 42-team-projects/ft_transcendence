1f177a36c3598b394fc086102468baa27bc48a0c
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-07-25 18:30:50 +0100

    use api for geting images of searching opponent

--- frontend/Game/GamePlay/SinglePlayer/SinglePlayerCart.js
+++ frontend/Game/GamePlay/SinglePlayer/SinglePlayerCart.js
@@ -1,47 +1,6 @@
 import { Lobby } from "../Lobby.js";
 
 const singlePlayerTemplate = document.createElement('template')
-const player_template = document.createElement('template')
-
-player_template.innerHTML = /*html*/ `
-
-<!-- .Player{
-    color: white;
-    font-size: clamp(1rem, 9vw, 10rem);
-} -->
-    <style>
-    .Player{
-        position: absolute;
-        height: 99%;
-        width: 99%;
-        z-index: 0;
-        border-radius: 2%;
-    }
-    .Opponent{
-        color: white;
-        font-size: clamp(1rem, 9vw, 10rem);
-    }
-    .Name{
-        z-index: 0;
-        position: absolute;
-        font-size: clamp(0.3rem, 1.5vw, 2rem);
-        color: white;
-        top:100%;
-        margin: 0;
-        animation: moveName 1s forwards;
-        transform: translateY(-200%);
-    }
-    @keyframes moveName {
-        to{
-            transform: translateY(0%);
-        }
-    }
-    </style>
-    <img id='Player' class="Player" slot="PlayerImg" alt="Player" />
-    <h1 id='NPlayer' class="Name" slot="PlayerName"></h1>
-    <h1 id='Opponent' class="Opponent" slot="searshing"></h1>
-    <h1 id='NOpponent' class="Name" slot="OpponentName"></h1>
-`
 
 singlePlayerTemplate.innerHTML = /*html*/ `
     <link rel="stylesheet" href="Game/GamePlay/SinglePlayer/Single-Player.css">
@@ -80,28 +39,12 @@ export class SinglePlayer extends HTMLElement{
         super();
         this.appendChild(singlePlayerTemplate.content.cloneNode(true))
         const button = this.querySelector('c-button')
-        const root = document.querySelector('root-content')
-		setTimeout(() => {
-
+		// setTimeout(() => {
             button.addEventListener('click', ()=>{
                 const lobby = new Lobby()
-                const p_img = player_template.content.getElementById('Player')
-                p_img.src = 'images/svg-header/profile.jpeg';
-                const p_h1 = player_template.content.getElementById('NPlayer')
-
-                p_h1.textContent = 'NOUAKHRO'
-
-                const o_img = player_template.content.getElementById('Opponent')
-                o_img.textContent = 'AI'
-                const o_h1 = player_template.content.getElementById('NOpponent')
-
-                o_h1.textContent = 'AI'
-
-                lobby.appendChild(player_template.content.cloneNode(true))
-                root.innerHTML = ``
-                root.appendChild(lobby)
+                lobby.SinglePlayer();
             })
-        }, 4000);
+        // }, 4000);
         this.classList.toggle('cart-animation', true)
         this.classList.toggle('opacity-0', true)
         setTimeout(() => {
