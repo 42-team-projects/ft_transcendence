f9c5c5c35df3c7757e7f5a2ec97e8321a120788c
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-25 15:57:28 +0100

    working on friends list components.

--- frontend/ChatComponents/ChatRoom/ReceiverComponent.js
+++ frontend/ChatComponents/ChatRoom/ReceiverComponent.js
@@ -1,12 +1,10 @@
+import { getLeagueColor } from "../../Utils/LeaguesData.js";
+
 export class ReceiverComponent extends HTMLElement {
 
     constructor() {
         super();
         this.attachShadow({mode: "open"});
-    }
-
-    connectedCallback() {
-        const hasCorner = this.hasAttribute("corner");
         this.shadowRoot.innerHTML = `
             <style>
                 :host {
@@ -19,15 +17,34 @@ export class ReceiverComponent extends HTMLElement {
                 .content {
                     margin-top: 20px;
                 }
-
             </style>
             <c-hexagon class="profile" width="48px" height="47px" apply="true" bcolor="aqua">
-                <img slot="content" draggable="false" src="./images/svg-header/profile.jpeg" width="48px">
+                <img slot="content" draggable="false" width="48px">
             </c-hexagon>
             <div class="content">
                 <slot ></slot>
             </div>
         `;
     }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector(".profile").bcolor = getLeagueColor(this.league);
+        this.shadowRoot.querySelector(".profile img").src = this.profileImage;
+    }
+
+    static observedAttributes = ["league", "profile-image"];
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "league")
+            this.shadowRoot.querySelector(".profile").bcolor = getLeagueColor(newValue);
+        else if (name === "profile-image")
+            this.shadowRoot.querySelector(".profile img").src = newValue;
+    }
+    
+    set league(val) { this.setAttribute("league", val);}
+    get league() { return this.getAttribute("league");}
+
+    set profileImage(val) { this.setAttribute("profile-image", val);}
+    get profileImage() { return this.getAttribute("profile-image");}
 
 }
