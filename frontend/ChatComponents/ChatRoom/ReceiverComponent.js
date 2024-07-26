1b6cc25dc1f3da17edd4c8c4962d08e229bbbc59
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-26 11:01:31 +0100

    add some animations to chat Components.

--- frontend/ChatComponents/ChatRoom/ReceiverComponent.js
+++ frontend/ChatComponents/ChatRoom/ReceiverComponent.js
@@ -10,7 +10,23 @@ export class ReceiverComponent extends HTMLElement {
                 :host {
                     display: flex;
                     padding: 5px;
+                    animation: slide-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+                    overflow: scroll;
                 }
+
+                :host::-webkit-scrollbar {
+                    display: none;
+                }
+            
+                @keyframes slide-right {
+                    0% {
+                      transform: translateX(-100%);
+                    }
+                    100% {
+                      transform: translateX(0);
+                    }
+                }
+
                 * {
                     margin: 0;
                 }
