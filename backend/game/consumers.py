1fa37d2170666c27fc70c61fecc7fc400dda45d6
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-07-29 19:47:06 +0100

    seding and reciving y

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,26 +1,31 @@
 
 import json
 from channels.generic.websocket import WebsocketConsumer
-from . clients import client 
+from . clients import Client , GameRoom
+queue = []
+rooms = []
 class GameConsumer(WebsocketConsumer) :
-    queue = []
     def connect(self):
         self.accept()
     def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
-        user_id = data.get('userId', None)
-        if user_id:
-            clientO = client(ws=self, id=user_id)
-            print(user_id)
-            self.queue.append(clientO)
-            if(len(self.queue) >= 2):
-                player1 = self.queue.pop()
-                player2 = self.queue.pop() 
-                print(f"Player 1: {player1.id}, Player 2: {player2.id}")
-                player1.ws.send(json.dumps({'id' : player2.id}))
-                player2.ws.send(json.dumps({'id' : player1.id}))
-        else:
-            print("No userId provided")
-            self.send(json.dumps({'message': 'No userId provided'}))
+        status = data.get('status', None)
+        print("status : ", status)
+        if status == 'searching':
+            user_id = data.get('userId', None)
+            client = Client(ws=self, id=user_id)
+            queue.append(client)
+            if(len(queue) >= 2):
+                player1 = queue.pop()
+                player2 = queue.pop() 
+                room = GameRoom(player1, player2)
+                room.find_opponent()
+                rooms.append(room)
+        elif status == 'startGame':
+            room_name = data.get('room_name', None)
+            room = [room for room in rooms if room.room_name == room_name]
+            room[0].sendData(data, self)
     def disconnect(self, close_code):
-        print("Client disconnected")
+        global queue
+        queue = [client for client in queue if client.ws != self]
+        print("Client disconnected", queue)
