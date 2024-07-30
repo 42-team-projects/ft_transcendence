383f0218836b9006e757282b4d666c8352c313d0
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-07-30 14:45:57 +0100

    save

--- frontend/app.js
+++ frontend/app.js
@@ -56,14 +56,12 @@ import { CustomButton } from "./TournamentComponents/CustomButton.js";
 import { TournamentsTable } from "./TournamentComponents/TournamentsTable.js";
 import { CreateTournament } from "./TournamentComponents/CreateTournament.js";
 import { QRCodeComponent } from "./TournamentComponents/QRCodeComponent.js";
-import { SettingContainer } from "./TournamentComponents/SettingContainer.js";
 import { PlayersAndStages } from "./TournamentComponents/PlayersAndStages.js";
 import { Visibility } from "./TournamentComponents/VisibillitySettings.js";
 
 
 customElements.define("visibillity-settings", Visibility);
 customElements.define("players-and-stages", PlayersAndStages);
-customElements.define("setting-container", SettingContainer);
 customElements.define("qrcode-component", QRCodeComponent);
 customElements.define("create-tournament", CreateTournament);
 customElements.define("tournaments-table", TournamentsTable);
