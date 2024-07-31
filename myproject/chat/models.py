8862bb54335f7111c861dbc8239ebe132df8629f
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-07-31 10:15:23 +0100

    save my work.

---
+++ myproject/chat/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ChatList(models.Model):
+    pass
+    # sender_id = models.ForeignKey(User, unique=True, null=True)
+    # receiver_id = models.ForeignKey(User, unique=True, null=True)
+    # friend_id = models.IntegerField()
+    # receiver_id = models.IntegerField()
+    # content = models.TextField()
+    # date = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return f"receiver is = {self.receiver_id}"
+
+class Chat(models.Model):
+    # sender_id = models.ForeignKey(User, unique=True, null=True)
+    # receiver_id = models.ForeignKey(User, unique=True, null=True)
+    sender_id = models.IntegerField()
+    receiver_id = models.IntegerField()
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"receiver is = {self.receiver_id}"
