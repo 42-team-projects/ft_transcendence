b8bdf5e7a335d656eddf7d47d36c591d00dbba98
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-08-01 15:45:36 +0100

    add serializers file and add some api in views file

--- myproject/chat/urls.py
+++ myproject/chat/urls.py
@@ -1,8 +1,29 @@
 
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.chat, name='chat')
+]
+
+
+urlpatterns = [
+    path('', views.chat, name='chat'),
+    
+    # path("", views.chatSender, name='chat'),
+    
+    path("list", views.list, name='list'),
+    path("add/", views.add, name='add'),
+    path("html/", views.html, name='html'),
+    
+    
+    
+    
+    
+    # path("add/", views.chat_sender_id, name='chat-sender_id'),
+    
+    # path("chat-list/", views.chat_list, name='chat-list'),
+    # path("<int:id>/", views.chat_by_id, name='chat-by-id'),
+    # path("create/", views.send, name='create')
 ]
