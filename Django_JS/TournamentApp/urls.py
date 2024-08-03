f5399eb17e724dbfb783bff7dcb68ecc279e5a7b
Your Name <you@example.com>
2024-08-03 18:05:15 +0100

    calculate score of tournament and take winner

--- Django_JS/TournamentApp/urls.py
+++ Django_JS/TournamentApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("launch_tournament/", views.launch_tournament_test, name='launch_tournament_test'),
     path("", views.index, name='index'),
     path("list_tournaments/", views.list_tournaments, name="list_tournaments"),
     path('<int:id>/', views.get_tournament_by_id, name='get_tournament_by_id'),
@@ -9,8 +10,7 @@ urlpatterns = [
 #     path('List_Players/', views.List_Players, name='List_Players'),
     path('player/<int:player_id>/', views.get_tournaments_by_player_id, name='get_tournaments_by_player_id'),
     path('available_tournaments/', views.get_available_tournaments, name='get_available_tournaments'),
-#     path('Player_Join_Tournament/', views.Player_Join_Tournament, name='Player_Join_Tournament'),
 #     path('player_leave_tournament/', views.player_leave_tournament, name='player_leave_tournament'),
     
-    path('tournamet/<int:tournamentId>/player/<int:playerId>/', views.player_join_tournament, name='player_join_tournament'),
+    path('tournament/<int:tournamentId>/player/<int:playerId>/', views.player_join_tournament, name='player_join_tournament'),
 ]
