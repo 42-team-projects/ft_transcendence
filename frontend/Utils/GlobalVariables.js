dee2d4f2df6419bbf55ff5ff893e44559c35e908
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-06 18:17:03 +0100

    extract GenerateRoundsGraph from TournamentComponent file into a seperate file and make it a web component.

--- frontend/Utils/GlobalVariables.js
+++ frontend/Utils/GlobalVariables.js
@@ -1,1 +1,2 @@
-// export const playerId = JSON.parse(localStorage.getItem('loggedInUser')).id;
+export const playerId = JSON.parse(localStorage.getItem('loggedInUser')).id;
+export const apiUrl = 'http://127.0.0.1:8000/TournamentApp/';
