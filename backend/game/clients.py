db45b8f08287d591402faeadc4283d1b2b1a6539
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-08-06 20:15:56 +0100

    end of day

--- backend/game/clients.py
+++ backend/game/clients.py
@@ -10,16 +10,17 @@ class GameRoom :
         self._active = False
         self._canvas_width = 300
         self._canvas_height = 150
+        self._game_loop = True
 
-    def find_opponent(self):
+    async def find_opponent(self):
         message = {
             'room_name': self._room_name,
             'opponent_id': self._client2.id
         }
-        self._client1.ws.send(json.dumps(message))
+        await self._client1.ws.send(json.dumps(message))
         message['opponent_id'] = self._client1.id
-        self._client2.ws.send(json.dumps(message))
-    def game_loop(self):
+        await self._client2.ws.send(json.dumps(message))
+    async def game_loop(self):
         #canvas
         canvas_width = self._canvas_width
         canvas_height = self._canvas_height
@@ -28,8 +29,8 @@ class GameRoom :
         ball_x = canvas_width / 2
         ball_y = canvas_height / 2
         ball_radius = 10
-        ball_dx = 1
-        ball_dy = 1
+        ball_dx = 8
+        ball_dy = 5
 
         #racquet
         player1_y = canvas_height / 2
@@ -37,11 +38,11 @@ class GameRoom :
         racquet_width = 5
         racquet_height = 90
         #game loop
-        while True:
+        while self._game_loop:
             ball_x += ball_dx
             ball_y += ball_dy
             if ball_x + ball_radius >= canvas_width or ball_x - ball_radius <= 0 :
-                break
+                raise Exception("Game Over")
             if ball_y + ball_radius >= canvas_height or ball_y - ball_radius <= 0:
                 ball_dy = -ball_dy
             if ball_x - ball_radius <= racquet_width and ball_y >= player1_y and ball_y <= player1_y + racquet_height:
@@ -49,7 +50,7 @@ class GameRoom :
             if ball_x + ball_radius >= canvas_width - racquet_width and ball_y >= player2_y and ball_y <= player2_y + racquet_height:
                 ball_dx = -ball_dx
             
-            data1 = {
+            self._data1 = {
                 'ball': {
                     'x': ball_x,
                     'y': ball_y,
@@ -64,7 +65,7 @@ class GameRoom :
                     'racquet_height': racquet_height,
                 },
             }
-            data2 = {
+            self._data2 = {
                 'ball': {
                     'x': canvas_width - ball_x,
                     'y': ball_y,
@@ -79,14 +80,38 @@ class GameRoom :
                     'racquet_height': racquet_height,
                 },
             }
-            self.sendData(data1, self._client1.ws)
-            self.sendData(data2, self._client2.ws)
-            asyncio.sleep(0.05)
-    def sendData(self, data, ws):
+            await asyncio.sleep(0.016)
+    async def sendin(self):
+        while self._game_loop:
+            # print(self._data1, self._data2)
+            await self.sendData(self._data1, self._client1.ws)
+            await self.sendData(self._data2, self._client2.ws)
+            await asyncio.sleep(0.05)
+
+    async def game_loops(self):
+        i = 0
+        while i < 5:
+            message = {
+                'status': 'RoundStart',
+                'round': i
+            }
+            await self._client1.ws.send(json.dumps(message))
+            await self._client2.ws.send(json.dumps(message))
+            self._game_loop = True
+            await asyncio.sleep(4)
+            try:
+                await asyncio.gather(self.game_loop(), self.sendin())
+            except Exception as e:
+                print(e)
+                self._game_loop = False
+                await self._client1.ws.send(json.dumps({'status': 'RoundOver'}))
+                await self._client2.ws.send(json.dumps({'status': 'RoundOver'}))
+            i += 1
+    async def sendData(self, data, ws):
         if ws == self._client1.ws:
-            self._client2.ws.send(json.dumps(data))
+            await self._client2.ws.send(json.dumps(data))
         else:
-            self._client1.ws.send(json.dumps(data))
+            await self._client1.ws.send(json.dumps(data))
     def set_player_y(self, ws, y):
         if ws == self._client1.ws:
             self._client1.y = y
@@ -110,6 +135,7 @@ class GameRoom :
         self._canvas_width = width
     def set_canvas_height(self, height):
         self._canvas_height = height
+
 class Client :
     def __init__(self, ws, id):
         self.ws = ws
