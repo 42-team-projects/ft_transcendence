a2f70be1e75dbea8d8c8bb64d6ec65ff23743fb8
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-10 14:25:44 +0100

    listing available tournaments.

--- frontend/TournamentComponents/TournamentsTable.js
+++ frontend/TournamentComponents/TournamentsTable.js
@@ -1,11 +1,42 @@
 import { convertTimeStampIntoDate } from "../Utils/Convertor.js";
+import { calculateTimeDifferents } from "../Utils/DateUtils.js";
 import { apiUrl, playerId } from "../Utils/GlobalVariables.js";
-import { TournamentComponent } from "./TournamentComponent.js";
 
 const cssContent = /*css*/`
 :host {
-    
+    width: 100%;
+    height: 100%; 
 }
+
+
+.mainContainer {
+    display: flex;
+    width: 100%;
+    height: calc(100% - 50px);
+    overflow-y: scroll;
+}
+
+h2 {
+    margin: 0;
+}
+
+.mainContainer::-webkit-scrollbar {
+    opacity: 0.7;
+    background-color: transparent;
+    width: 2px;
+}
+
+.mainContainer::-webkit-scrollbar-track {
+    opacity: 0.7;
+    border-radius: 100px;
+}
+
+.mainContainer::-webkit-scrollbar-thumb {
+    opacity: 0.7;
+    background-color: aqua;
+    border-radius: 100px;
+}
+
 
 table {
     width: 100%;
@@ -39,6 +70,7 @@ tbody > tr {
 }
 
 td {
+    height: 64px;
     border-top: 0.1px solid #051d31;
 }
 
@@ -48,8 +80,92 @@ td {
     align-items: center;
     justify-content: center;
 }
+
+
+.tournament-actions {
+    width: 100%;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    padding: 5px;
+}
+
+.settingsContainer {
+    width: 100%;
+    height: 100%;
+    background: green;
+    display: flex;
+}
+.settings-section {
+    flex: 2;
+    height: 100%;
+    background: #cccc4040;
+}
+
+.qrcode-section {
+    flex: 1;
+    height: 100%;
+    background: #ffffff40;
+}
+
+.rounds,
+.final {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+
+.final {
+    flex-direction: row;
+    gap: 20px;
+}
+
+.rounds > tournament-round > div {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    max-width: 200px;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+}
+
+.c-hexagon-content {
+    width: 100%;
+    height: 100%;
+}
+h4 {
+    max-width: 90px;
+    overflow-x: scroll;
+}
+
+h4::-webkit-scrollbar {
+    display: none;
+}
+
 `;
 
+const TABLEHEADER = `
+<table cellspacing="0" cellpadding="0">
+    <thead>
+        <tr>
+            <th>NAME</th>
+            <th>OWNER</th>
+            <th>CREATED AT</th>
+            <th>NUMBER OF PLAYERS</th>
+            <th>DEADLINE</th>
+            <th>ACCESS</th>
+            <th>ACTIONS</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+`;
 
 export class TournamentsTable extends HTMLElement {
     constructor() {
@@ -59,23 +175,21 @@ export class TournamentsTable extends HTMLElement {
             <style>
                 ${cssContent}
             </style>
-            <table cellspacing="0" cellpadding="0">
-                <thead>
-                    <tr>
-                        <th>NAME</th>
-                        <th>OWNER</th>
-                        <th>BEGIN</th>
-                        <th>NUMBER OF PLAYERS</th>
-                        <th>STATE</th>
-                        <th>ACCESS</th>
-                        <th>ACTIONS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
+            <div class="mainContainer">
+                ${TABLEHEADER}
+            </div>
+
+            <div class="tournament-actions">
+                <custom-button id="firstButton" width="300px">
+                    <h3>JOIN TOURNAMENT</h3>
+                </custom-button>
+                <custom-button id="secondButton" width="300px" background-color="#0C9BA3" reverse>
+                    <h3>CREATE TOURNAMENT</h3>
+                </custom-button>
+            </div>
         `;
     }
+
 
     createRow(data) {
         const tr = document.createElement("tr");
@@ -105,7 +219,9 @@ export class TournamentsTable extends HTMLElement {
         }
         {
             const td = document.createElement("td");
-            td.textContent = "STARTED";
+            setInterval(() => {
+                td.textContent = calculateTimeDifferents(data.created_at);
+            }, 1000);
             tr.appendChild(td);
         }
         {
@@ -140,11 +256,37 @@ export class TournamentsTable extends HTMLElement {
         }
     }
 
-    async connectedCallback() {
+
+
+    async createTournament(data)
+    {
+        try {
+            const Tournament = {
+                tournament_name: data.name,
+                number_of_players: data.num_players,
+                is_accessible: data.access.toLowerCase() == "public" ? true : false,
+                access_password: data.password
+            }
+            const create_tournament = "create/player/";
+            const response = await fetch(`${apiUrl}${create_tournament}${playerId}/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(Tournament),
+            });
+            if (!response.ok)
+                throw new Error(`${response.status}  ${response.statusText}`);
+            return await response.json();
+        } catch(error) {
+            console.error('Error creating Tournament: ', error);
+        }
+    }
+
+    async createTournamentTable() {
         const APIData = await this.get_tournaments_by_player_id();
         if (!APIData)
             return;
-        console.log(APIData);
         const tbody = this.shadowRoot.querySelector("tbody");
 
         for (let index = APIData.length - 1; index >= 0; index--)
@@ -164,6 +306,61 @@ export class TournamentsTable extends HTMLElement {
                 this.shadowRoot.appendChild(rounds);
             });
         });
+    }
+
+    async connectedCallback() {
+
+
+        this.createTournamentTable();
+
+
+        const mainContainer = this.shadowRoot.querySelector(".mainContainer");
+        const firstButton = this.shadowRoot.getElementById("firstButton");
+        const secondButton = this.shadowRoot.getElementById("secondButton");
+        firstButton.addEventListener("click", () => {
+            const buttonValue = firstButton.querySelector("h3");
+            if (buttonValue.textContent == "CANCEL") {
+                mainContainer.innerHTML = TABLEHEADER;
+                this.createTournamentTable();
+                buttonValue.textContent = "JOIN TOURNAMENT";
+                secondButton.querySelector("h3").textContent = "CREATE TOURNAMENT";
+            }
+            else {
+
+            }
+        });
+        secondButton.addEventListener("click", async () => {
+            const buttonValue = secondButton.querySelector("h3");
+            if (buttonValue.textContent == "GENERATE") {
+                const data = this.shadowRoot.querySelector("create-tournament").data;
+                if (data)
+                {
+                    try {
+                        const response = await this.createTournament(data);
+                        if (!response)
+                            throw new Error(`${response.status}  ${response.statusText}`);
+                        const tournamentResponse = await response.tournament;
+                        this.shadowRoot.innerHTML = '';
+                        const rounds = document.createElement("generate-rounds");
+                        rounds.numberOfPlayers = tournamentResponse.number_of_players;
+                        rounds.players = tournamentResponse.players;
+                        this.shadowRoot.appendChild(rounds);
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+                else
+                    console.log("Please fill all inputs field!!");
+            }
+            else
+            {
+                mainContainer.innerHTML = '';
+                mainContainer.appendChild(document.createElement("create-tournament"));
+                buttonValue.textContent = "GENERATE";
+                firstButton.querySelector("h3").textContent = "CANCEL";
+            }
+        });
+    
     }
 
     disconnectedCallback() {
