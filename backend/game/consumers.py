cdcbfed235bb3eee90b11a696225dd6cf848904e
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-08-12 22:56:17 +0100

    end of day

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -2,70 +2,43 @@ import time
 import asyncio
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from . clients import Client , GameRoom
+from . clients import GameLoop
 queue = []
-rooms = []
+
+
+
+
 class GameConsumer(AsyncWebsocketConsumer) :
     async def connect(self):
+        self.id = self.scope['url_route']['kwargs']['id']
+        await self.add_to_queue()
+        if len(queue) >= 2:
+            self.game_loop = GameLoop(self)
+            client1 = queue.pop(0)
+            queue.pop(0)
+            await self.add_to_room(client1)
         await self.accept()
     async def receive(self, text_data=None, bytes_data=None):
-        start = time.time()
-        data = json.loads(text_data)
-        status = data.get('status', None)
-        if status == 'P2245laye':
-            if status == 'searching':
-                user_id = data.get('userId', None)
-                await self.add_to_queue(user_id)
-                if(len(queue) >= 2):
-                    await self.add_to_room()
-            elif status == 'startGame':
-                room_name = data.get('room_name', None)
-                room = self.get_room_by_name(room_name)
-                room.set_canvas_width(data.get('canvas_width', None))
-                room.set_canvas_height(data.get('canvas_height', None))
-                room.set_racquet_size(data.get('racquet_size', None),)
-                if(room._active == False):
-                    asyncio.create_task(room.game_loops())
-                    room._active = True
-            elif status == 'move':
-                room_name = data.get('room_name', None)
-                room = self.get_room_by_name(room_name)
-                y = data.get('position', None)
-                if room:
-                    await room.set_player_y(self, y)
-                    message = {
-                        'status': 'move',
-                        'opponent_y': y
-                    }
-                    await room.sendData(message, self)
-
+        pass
     async def disconnect(self, close_code):
-        global queue
-        global rooms
-        queue = [client for client in queue if client.ws != self]
-        current_room = self.get_room_by_client(self)
-        if current_room:
-            await current_room.handle_disconnect()
-        rooms = [room for room in rooms if room != current_room]
+        await self.channel_layer.group_discard(
+            self.room_name,
+            self.channel_name
+        )
+    
+    async def join_room(self, client, room_name):
+        await self.channel_layer.group_add(
+            room_name,
+            client.channel_name
+        )
+        
+    async def add_to_room(self, client1):
+        room_group_name = f'game_{client1.id}-{self.id}'
+        await self.join_room(client1, room_group_name)
+        await self.join_room(self, room_group_name)
 
-    async def add_to_queue(self, user_id):
-        client = Client(ws=self, id=user_id)
-        queue.append(client)
-
-    async def add_to_room(self):
-        client2 = queue.pop()
-        client1 = queue.pop()
-        room = GameRoom(client1, client2)
-        rooms.append(room)
-        await room.get_opponent()
-
-    def get_room_by_name(self, room_name):
-        for room in rooms:
-            if room.get_room_name() == room_name:
-                return room
-        return None
-    def get_room_by_client(self, ws):
-        for room in rooms:
-            if room.get_client1_ws() == ws or room.get_client2_ws() == ws:
-                return room
-        return None
+    async def game_message(self, event):
+        message = event['message']
+        self.send(json.dumps(message))
+    async def add_to_queue(self):
+        queue.append(self)
