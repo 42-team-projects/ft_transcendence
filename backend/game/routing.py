3746eaed0db0a8344d5b092865533d302b0141ca
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-08-12 22:56:17 +0100

    end of day

--- backend/game/routing.py
+++ backend/game/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .consumers import GameConsumer
 
 ws_urlpatterns = [
-    path('ws/game/', GameConsumer.as_asgi())
+    path("ws/game/<str:id>/", GameConsumer.as_asgi()),
 ]
