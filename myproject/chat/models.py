a99516a7f22c7b51eded5b3ce591a6fcf36476b8
m-zeroual <mohammedzeroual200@gmail.com>
2024-08-14 19:30:16 +0100

    create chat app

--- myproject/chat/models.py
+++ myproject/chat/models.py
@@ -1,30 +1,32 @@
 from django.db import models
+# from django.core.exceptions import ValidationError
 
-# Create your models here.
-
-
-class ChatList(models.Model):
-    pass
-    # sender_id = models.ForeignKey(User, unique=True, null=True)
-    # receiver_id = models.ForeignKey(User, unique=True, null=True)
-    # friend_id = models.IntegerField()
-    # receiver_id = models.IntegerField()
-    # content = models.TextField()
-    # date = models.DateTimeField(auto_now_add=True)
-
-    # def __str__(self):
-    #     return f"receiver is = {self.receiver_id}"
-    
-
-
-
-class Chat(models.Model):
-    # sender_id = models.ForeignKey(User, unique=True, null=True)
-    # receiver_id = models.ForeignKey(User, unique=True, null=True)
-    sender_id = models.IntegerField()
-    receiver_id = models.IntegerField()
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+class User(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"receiver is = {self.receiver_id}"
+        return self.name
+
+class Conversation(models.Model):
+    users = models.ManyToManyField(User, related_name='users')
+    conversation_name = models.CharField(max_length=255, unique=True)
+    # last_message = models.ForeignKey(Message, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.conversation_name
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    # def save(self, *args, **kwargs):
+    #     conv = Conversation.objects.filter(users=self.sender ,conversation_name=self.conversation.conversation_name)
+    #     if not conv:
+    #         raise ValidationError(f"{self.sender.name} is not part of the conversation {self.conversation.conversation_name}.")
+    #     super().save(*args, **kwargs)
+    def __str__(self):
+        return f'{self.sender.name} send message to conversation {self.conversation.conversation_name}'
+
--- myproject/chat/views.py
+++ myproject/chat/views.py
@@ -1,86 +1,111 @@
-
-
-
-
-
-'''
-from django.shortcuts import render
-from rest_framework.response import Response
-
-# Create your views here.
-
-
-def chat(request, name):
-    return Response({ "name" : name})
-    
-def defualt(request):
-    return Response({"name" : "mzeroual"})
-
-'''
 from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Chat
-from .serializers import ChatSerializer
+from .models import *
+from .serializers import *
 from django.db.models import Q
-
+import json
+from django.core.serializers import serialize
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
 
 
 def chat(request):
     return render(request, 'chat/chat.html')
 
-# @api_view(['GET'])
-# def chat_view_param(request, name):
-#     return Response({"user": name})
+@api_view(['GET'])
+def get_all_messages(request, conversation_name):
+    conversation = get_object_or_404(Conversation, conversation_name=conversation_name)
+    serializer = ConversationSerializer(conversation)
+    messages = Message.objects.filter(conversation=serializer.data['id'])
+        
+    if messages.exists():
+        # Serialize the queryset
+        message_serializer = MessageSerializer(messages, many=True)
+        return Response(message_serializer.data)
+    else:
+        return Response({'Error': 'No messages found for this conversation'}, status=404)
 
 
-# def chat_view(request):
-#     return render(request, 'app/index.html')
-
-# @api_view(['GET'])
-# def chat(request) -> Response:
-#     chats = Chat.objects.all()
-#     serializer = ChatSerializer(chats, many=True)
-#     return Response(serializer.data)
 
 @api_view(['GET'])
-def chatSender(request) -> Response:
-    sender_id = request.GET.get('sender_id')
-    receiver_id = request.GET.get('receiver_id')
-    chats = Chat.objects.filter(Q(sender_id=sender_id, receiver_id=receiver_id) | Q(sender_id=receiver_id, receiver_id=sender_id))
-    serializer = ChatSerializer(chats, many=True)
-    return Response(serializer.data)
+def get_all_conversations(request):
+    conversations = Conversation.objects.all() 
+    if conversations.exists():
+        # Serialize the queryset
+        conversations_serializer = ConversationSerializer(conversations, many=True)
+        return Response(conversations_serializer.data)
+    else:
+        return Response({'Error': 'No conversations found'}, status=404)
 
 @api_view(['GET'])
-def list(request) -> Response:
-    chats = Chat.objects.all()
-    serializer = ChatSerializer(chats, many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def add(request) -> Response:
-    serializer = ChatSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-def html(request):
-    return render(request, "chat/index.html")
-
-
-def getAllMessages():
-    pass
-
-# def chat_list(request) -> Response:
-#     chats = Chat.objects.all()
-#     serializer = ChatSerializer(chats, many=True)
-#     return Response(serializer.data)
-
-# @api_view(['GET'])
-# def chat_by_id(request, id) -> Response:
-#     chats = Chat.objects.get(id=id)
-#     serializer = ChatSerializer(chats, many=False)
+def last_message(request, conversation_name):
+    conversation = get_object_or_404(Conversation)
+    message = Message.objects.filter(conversation=conversation.id).last()
+    if message:
+        message_serializer = MessageSerializer(message)
+        return Response(message_serializer.data)
+    else:
+        return Response({'Error': 'No message found'}, status=404)
     
+
+
+
+# # @api_view(['GET'])
+# # def chat_view_param(request, name):
+# #     return Response({"user": name})
+
+
+# # def chat_view(request):
+# #     return render(request, 'app/index.html')
+
+# # @api_view(['GET'])
+# # def chat(request) -> Response:
+# #     chats = Chat.objects.all()
+# #     serializer = ChatSerializer(chats, many=True)
+# #     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def chatSender(request) -> Response:
+#     sender_id = request.GET.get('sender_id')
+#     receiver_id = request.GET.get('receiver_id')
+#     chats = Message.objects.filter(Q(sender_id=sender_id, receiver_id=receiver_id) | Q(sender_id=receiver_id, receiver_id=sender_id))
+#     serializer = ChatSerializer(chats, many=True)
 #     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def list(request) -> Response:
+#     chats = Friend.objects.all()
+#     print(chats.order_by('id'))
+#     chats = Message.objects.all()
+#     serializer = ChatSerializer(chats, many=True)
+#     # print(serializer.data)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def add(request) -> Response:
+#     serializer = ChatSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+
+# def html(request):
+#     return render(request, "chat/index.html")
+
+
+# def getAllMessages():
+#     pass
+
+# # def chat_list(request) -> Response:
+# #     chats = Chat.objects.all()
+# #     serializer = ChatSerializer(chats, many=True)
+# #     return Response(serializer.data)
+
+# # @api_view(['GET'])
+# # def chat_by_id(request, id) -> Response:
+# #     chats = Chat.objects.get(id=id)
+# #     serializer = ChatSerializer(chats, many=False)
+    
+# #     return Response(serializer.data)
