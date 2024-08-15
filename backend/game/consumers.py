333a13d967709b385eb0ab81a1e683cbd12983c6
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-08-15 11:22:08 +0100

    end of day

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -11,7 +11,7 @@ async def join_room(client):
         client.room_group_name,
         client.channel_name
     )
-	
+
 async def add_to_room(opponent, controler):
 	await join_room(opponent)
 	await join_room(controler)
@@ -26,18 +26,15 @@ async def remove_from_queue(client):
 
 async def timer(client, seconds):
     for i in range(seconds):
-        message = f"Time left: {seconds - i}"
-        client.send_message(message)
+        message = { 'time': seconds - (i + 1) }
+        await client.send_message(message, 'timer_message')
 #always the second client will be the controler
 #always the first client will be the opponent
-from channels.generic.websocket import AsyncWebsocketConsumer
-import json
-import asyncio
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        GameConsumer.rooms = {}
         self.id = self.scope['url_route']['kwargs']['id']
-        self.room_group_name = 'hiii'
 
         add_to_queue(self)
         await self.accept()
@@ -46,14 +43,27 @@ class GameConsumer(AsyncWebsocketConsumer):
         except asyncio.TimeoutError:
             print('Timeout')
             await self.close()
-        await asyncio.sleep(6)
-        timer(self, 3)
 
-    async def send_message(self, message):
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        if text_data_json['message'] == 'start':
+            GameConsumer.rooms[self.room_group_name] += 1
+            if GameConsumer.rooms[self.room_group_name] == 2 :
+                # await self.send_message('start')
+                await timer(self, 4)
+
+    async def disconnect(self, close_code):
+        await remove_from_queue(self)
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+    async def send_message(self, message, function='game_message'):
+        print(f"Sending message: {message}, function: {function}")
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'game_message',
+                'type': function,
                 'message': message
             }
         )
@@ -62,18 +72,23 @@ class GameConsumer(AsyncWebsocketConsumer):
         
         while len(queue) < 2 and self in queue:
             await asyncio.sleep(0.1)
+
         if len(queue) >= 2:
             opponent = queue.pop(0)
             controler = queue.pop(0)
+            controler.room_group_name = f"game_{controler.id}-{opponent.id}"
+            opponent.room_group_name = f"game_{controler.id}-{opponent.id}"
             await add_to_room(opponent, controler)
             message = {
+                'text': 'Opponent found',
                 'user_id_1': opponent.id,
                 'user_id_2': controler.id,
             }
             await self.send_message(message=message)
+            GameConsumer.rooms[self.room_group_name] = 0
 
     async def timer_message(self, event):
-        self.send_message(event)
+        await self.game_message(event)
         await asyncio.sleep(1)
     
     async def game_message(self, event):
