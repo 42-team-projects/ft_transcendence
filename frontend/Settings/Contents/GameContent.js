ad986d3fc71fd0d072582c60006839c9a647c382
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-20 19:56:04 +0000

    finish game settings.

--- frontend/Settings/Contents/GameContent.js
+++ frontend/Settings/Contents/GameContent.js
@@ -1,4 +1,5 @@
 import { CustomSliders } from "../CustomSliders.js"
+import { CustomField } from "../CustomField.js"
 
 
 export class GameContent extends HTMLElement {
@@ -7,18 +8,51 @@ export class GameContent extends HTMLElement {
         this.attachShadow({
             mode: "open"
         });
-    }
-
-    connectedCallback() {
         this.shadowRoot.innerHTML = `
             <style> ${cssContent} </style>
             <div class="container">
-                <custom-sliders label="GAME PLAY"></custom-sliders>
+                <custom-sliders label="GAME PLAY" game-color="#00fffc"></custom-sliders>
+                <custom-field label="GAME PLAY COLOR" description="chose the color you want play on it.">
+                    <input type="color" id="gamePlayColor" name="body" value="#00fffc" />
+                </custom-field>
+                <custom-field label="FIRST RACKET" description="chose the color of the first racket.">
+                    <input type="color" id="firstRacketColor" name="body" value="#f6b73c" />
+                </custom-field>
+                <custom-field label="SECOND RACKET" description="chose the color of the second racket.">
+                    <input type="color" id="secondRacketColor" name="body" value="#b23def" />
+                </custom-field>
             </div>
             <div class="actions">
                 <settings-item color="aqua" border-size="2px" width="64px" height="40px"><h4>SAVE</h4></settings-item>
             </div>
         `;
+    }
+
+    interval;
+
+    connectedCallback() {
+        const gamePlayColorSelector = this.shadowRoot.querySelector("#gamePlayColor");
+        let gamePlayColor = gamePlayColorSelector.value;
+        const customSliders = this.shadowRoot.querySelector("custom-sliders");
+        gamePlayColorSelector.addEventListener("click", () => {
+            let counter = 0;
+            if (this.interval)
+                clearInterval(this.interval);
+            this.interval = setInterval(() => {
+                if (gamePlayColorSelector.value != gamePlayColor)
+                {
+                    gamePlayColor = gamePlayColorSelector.value;
+                    customSliders.gameColor = gamePlayColor;
+                    counter = 0;
+                }
+                if (counter >= 3000)
+                {
+                    clearInterval(this.interval);
+                }
+                console.log("hello from game play selector : ", this.interval);
+                counter += 100;
+            }, 100);
+        });
     }
 
     disconnectedCallback() {
@@ -57,18 +91,6 @@ const cssContent = /*css*/`
         width: 90%;
         align-items: center;
         justify-content: space-around;
-    }
-
-    h3 {
-        color: white;
-    }
-
-    .c-hexagon-content {
-        width: 100%;
-        height: 100%;
-        background: url("../../assets/profile-assets/tanjuro.jpg");
-        background-position: center;
-        background-size: cover;
     }
 
 `;
