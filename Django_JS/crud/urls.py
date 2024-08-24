57f76a118b4fb51dfdec851e03fc49060d0c4dd5
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-08-24 15:01:27 +0100

    befor merging

--- Django_JS/crud/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Get_Post_Methods, name='Get_Post'),
-    path('<int:id>/', views.Put_Delete_Methods, name='Put_Delete'),
-]
