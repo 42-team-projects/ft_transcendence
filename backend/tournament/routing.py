6da238beab4d2238e91335d480604850c998bcb5
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-24 16:49:52 +0100

    convert rest api into websocket in tournament pages.

--- backend/tournament/routing.py
+++ backend/tournament/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('tournament/<int:tournament_id>/', consumers.TournamentConsumer.as_asgi()),
+    path('tournament/sync/<str:tournament_name>/', consumers.TournamentDataSyncConsumer.as_asgi()),
 ]
