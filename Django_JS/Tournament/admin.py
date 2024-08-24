899494a7042a37c580cebfa5656e8d8070f4df57
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-24 16:49:52 +0100

    convert rest api into websocket in tournament pages.

--- Django_JS/Tournament/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import  Tournament, Stage, Player
-# Register your models here.
-
-admin.site.register(Player)
-admin.site.register(Tournament)
-admin.site.register(Stage)
-# admin.site.register(PlayerTournament)
