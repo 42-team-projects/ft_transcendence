ef44f674a70c6daa7d417f33740b6194be8318ef
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-25 19:14:29 +0100

    add notification component into header component.

--- frontend/Notification/NotificationComponent.js
+++ frontend/Notification/NotificationComponent.js
@@ -4,7 +4,8 @@ export class NotificationComponent extends HTMLElement {
         this.attachShadow({mode: "open"});
         this.shadowRoot.innerHTML = `
             <style> ${cssContent} </style>
-            <div class="parent-box">
+            <div class="parent-box active"></div>
+            <div class="content">
                 <div class="top-line"></div>
                 <div class="main-box">
                     <div class="mainContainer">
@@ -19,7 +20,10 @@ export class NotificationComponent extends HTMLElement {
     connectedCallback() {
         this.style.width = this.width;
         this.style.height = this.height;
-
+        // const interval = setInterval(() => {
+        //     clearInterval(interval);
+        //     this.remove();
+        // }, 5000);
     }
 
     disconnectedCallback() {
@@ -46,24 +50,69 @@ export class NotificationComponent extends HTMLElement {
 customElements.define("notification-component", NotificationComponent);
 
 const cssContent = /*css*/`
+/* Google Fonts - Poppins */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 :host {
     width: 100%;
-    height: 96px;
+    min-height: 100px;
+    height: auto;
     flex: 1;
     display: flex;
     align-items: center;
     justify-content: center;
-    max-width: 600px;
+    min-width: 200px;
+    position: relative;
     box-shadow: 2px 2px 10px 2px #00fffc40, inset 2px 2px 10px 2px #00fffc40;
+    animation: slide-right 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
 
 .parent-box {
     width: 100%;
+    min-height: 100px;
     height: 100%;
     border: 1px solid #00fffc80;
-    clip-path: polygon(100% 0, 100% calc(100% - 32px), calc(100% - 24px) 100%, 0 100%, 0 0);
     position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    clip-path: polygon(0% 0%, 0% 100%, 2px 100%, 2px 2px, calc(100% - 2px) 2px, calc(100% - 2px) calc(100% - 2px), 2px calc(100% - 2px), 2px 100%, 100% 100%, 100% 0%);
+    justify-content: center;
+    background: #02253b;
+}
+
+.parent-box::before {
+    content: "";
+    position: absolute;
+    height: 1000px;
+    width: 1000px;
+    border-radius: 100%;
+    background-image: conic-gradient(aqua, transparent, transparent, transparent, transparent, transparent, transparent);
+}
+
+.parent-box.active::before {
+    animation: rotate 8s linear infinite;
+}
+
+
+@keyframes rotate {
+    0% {
+        transform: rotate(360deg);
+    }
+    100% {
+        transform: rotate(0deg);
+    }
+}
+
+.content {
+    position: absolute;
+    width: calc(100% - 4px);
+    height: calc(100% - 4px);
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -104,5 +153,31 @@ const cssContent = /*css*/`
     width: calc(100% - 3px);
     height: calc(100% - 3px);
 }
+
+::slotted(div) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+}
+
+@keyframes slide-right {
+    0% {
+      transform: translateY(-400px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+}   
+
+@keyframes slide-left {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-400px);
+    }
+}   
 
 `;
