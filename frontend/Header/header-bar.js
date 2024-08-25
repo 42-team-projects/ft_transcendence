5193c28c71c5727bfcb1c4ff8ff91e8c4402be44
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-25 13:21:29 +0100

    create notification component.

--- frontend/Header/header-bar.js
+++ frontend/Header/header-bar.js
@@ -1,3 +1,5 @@
+import { NotificationComponent } from "../Notification/NotificationComponent.js";
+
 const HeaderTemplate =  document.createElement('template');
 
 HeaderTemplate.innerHTML = /*html*/`
@@ -5,10 +7,20 @@ HeaderTemplate.innerHTML = /*html*/`
         <img loading="lazy" draggable="false" src="./images/svg-header/pingpong-icon.svg" alt="pingpong">
     </div>
     <div class="notification-search">
-        <search-bar></search-bar>
-        <div class="notification-icon" >
-            <img loading="lazy" draggable="false" src="./images/svg-header/alarm.svg" alt="notification">
+        <div class="search-box">
+            <search-bar></search-bar>
+            <div class="notification-icon" >
+                <img loading="lazy" draggable="false" src="./images/svg-header/alarm.svg" alt="notification">
+            </div>
         </div>
+        <notification-component width="30%">
+            <div class="notification-content">
+                <c-hexagon class="profile" width="56px" height="55px" apply="true" bcolor="#d9d9d9">
+                    <div slot="content" class="c-hexagon-content"></div>
+                </c-hexagon>
+                <h3>hello world</h3>
+            </div>
+        </notification-component>
     </div>
     <c-profile></c-profile>
     `
