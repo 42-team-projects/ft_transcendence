43f1694ec573926bd403676da193e368d9a07a5b
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-08-26 16:56:30 +0100

    save

--- ft_transcendence/notification/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-
-websocket_urlpatterns = [
-    re_path(r'ws/notification/user/(?P<room_name>\w+)/$', consumers.UserNotificationConsumer.as_asgi()),
-    # re_path(r'ws/notification/group/(?P<room_name>\w+)/$', consumers.UserNotificationConsumer.as_asgi())
-]
