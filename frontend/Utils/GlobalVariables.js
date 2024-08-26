4b9734abad8a0612a5f7f6517c088034907d05de
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-26 10:39:45 +0000

    update root.js file

--- frontend/Utils/GlobalVariables.js
+++ frontend/Utils/GlobalVariables.js
@@ -3,3 +3,10 @@ export const playerId = JSON.parse(localStorage.getItem('loggedInUser')).id;
 // export const apiUrl = 'http://127.0.0.1:8000/tournament/';
 export const apiUrl = 'http://127.0.0.1:8000/tournament/'
 export const wsUrl = 'ws://127.0.0.1:8000/';
+
+export const ip = '10.11.3.7';
+const config = {
+    serverIP: ip,
+};
+
+export default config;
