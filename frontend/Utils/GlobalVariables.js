fbadda2ac8c100f8d89c41e80b863077c131fa06
nourdine-ouakhroun <nourdineouakhroun@gmail.com>
2024-08-26 15:31:29 +0100

    handling errors in routing

--- frontend/Utils/GlobalVariables.js
+++ frontend/Utils/GlobalVariables.js
@@ -3,3 +3,9 @@ export const playerId = 2;
 // export const apiUrl = 'http://127.0.0.1:8000/tournament/';
 export const apiUrl = 'http://127.0.0.1:8000/tournament/'
 export const wsUrl = 'ws://127.0.0.1:8000/';
+
+// set acsses token in local storage
+localStorage.setItem('accessToken', 'token');
+// get acsses token in local storage
+export const accessToken = localStorage.getItem('accessToken');
+console.log(accessToken);
--- frontend/root/root.js
+++ frontend/root/root.js
@@ -2,7 +2,6 @@ import { ChatComponent } from "../Chat/ChatComponent.js";
 import { ProfileComponent } from "../Profile/ProfileComponent.js";
 import { TournamentComponent } from "../Tournament/TournamentComponent.js";
 import { SettingsComponent } from "../Settings/SettingsComponent.js";
-import { router } from "../User/Router.js";
 
 const root = document.createElement('template')
 
@@ -35,58 +34,62 @@ class Root extends HTMLElement{
         super();
         this.appendChild(root.content.cloneNode(true))
     }
-
-    set ChangeRootContent(component){
-        const content = document.createElement(component)
-        this.innerHTML = ``
-        this.appendChild(content);
+    changeStyle(){
+        document.body.classList.add('body-default-shrink')
+        header.render()
+        sideBar.render()
+        // document.body.querySelector('footer').render()
     }
+    // set ChangeRootContent(component){
+    //     const content = document.createElement(component)
+    //     this.innerHTML = ``
+    //     this.appendChild(content);
+    // }
 
     clickEvent() {
         
-        // const buttons = sideBar.shadowRoot.querySelectorAll('sb-button')
-        // buttons.forEach((button, index) => {
-        //     button.addEventListener('click', () => {
-        //         if(button.classList.length === 0)
-        //         {
-        //             this.ChangeRootContent = rootContent[index]
-        //             sideBar.clickEvent = index;
-        //             sideBar.activeButton = button;
-        //         }
-        //     });
-        // })
-        // const profile = header.querySelector('c-profile')
-        // const userRunk = header.querySelector('user-rank');
-        // profile.addEventListener('click', () => {
-        //     if(this.firstChild.nodeName !== 'PROFILE-COMPONENT')
-        //         this.ChangeRootContent = 'profile-component'
-        //     if(sideBar.activeButton.classList.length)
-        //     {
-        //         userRunk.classList.toggle('drop-100', false);
-        //         userRunk.classList.toggle('transform-1s', true);
-        //         userRunk.classList.toggle('down-60', false);
-        //         userRunk.classList.toggle('rise-0', true);
-        //         sideBar.activeButton.classList.toggle('on')
-        //         sideBar.activeButton.shadowRoot.querySelector('sb-icon').classList.toggle('on')
-        //         sideBar.activeButton.shadowRoot.querySelector('.c-sb-text').classList.toggle('on')
-        //         sideBar.activeButton.querySelector('h1').classList.toggle('on')
-        //         sideBar.activeButton.querySelector('img').classList.toggle('on')
-        //     }
-        // })
-        // const logout = footer.querySelector('.logout')
+        const buttons = sideBar.shadowRoot.querySelectorAll('sb-button')
+        buttons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                if(button.classList.length === 0)
+                {
+                    this.ChangeRootContent = rootContent[index]
+                    sideBar.clickEvent = index;
+                    sideBar.activeButton = button;
+                }
+            });
+        })
+        const profile = header.querySelector('c-profile')
+        const userRunk = header.querySelector('user-rank');
+        profile.addEventListener('click', () => {
+            if(this.firstChild.nodeName !== 'PROFILE-COMPONENT')
+                this.ChangeRootContent = 'profile-component'
+            if(sideBar.activeButton.classList.length)
+            {
+                userRunk.classList.toggle('drop-100', false);
+                userRunk.classList.toggle('transform-1s', true);
+                userRunk.classList.toggle('down-60', false);
+                userRunk.classList.toggle('rise-0', true);
+                sideBar.activeButton.classList.toggle('on')
+                sideBar.activeButton.shadowRoot.querySelector('sb-icon').classList.toggle('on')
+                sideBar.activeButton.shadowRoot.querySelector('.c-sb-text').classList.toggle('on')
+                sideBar.activeButton.querySelector('h1').classList.toggle('on')
+                sideBar.activeButton.querySelector('img').classList.toggle('on')
+            }
+        })
+        const logout = footer.querySelector('.logout')
 
-        // logout.addEventListener('click', () => {
+        logout.addEventListener('click', () => {
 
-        //     this.ChangeRootContent = rootContent[0]
-        //     sideBar.clickEvent = 0;
-        //     sideBar.activeButton = buttons[0];
-        // })
+            this.ChangeRootContent = rootContent[0]
+            sideBar.clickEvent = 0;
+            sideBar.activeButton = buttons[0];
+        })
     }
-    // connectedCallback()
-    // {
-    //     // this.ChangeRootContent = "home-page";
-    //     // this.clickEvent()
-    // }
+    connectedCallback()
+    {
+        this.clickEvent()
+    }
 }
 customElements.define("root-content", Root)
 
