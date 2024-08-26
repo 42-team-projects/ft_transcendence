a5bd409fd5964ebcbbbf7b20f4bb5e5a374c0fd2
m-zeroual <mohammedzeroual200@gmail.com>
2024-08-26 11:28:18 +0100

    simple change in chat application

--- ft_transcendence/chat/serializers.py
+++ ft_transcendence/chat/serializers.py
@@ -13,39 +13,21 @@ class MessageSerializer(serializers.ModelSerializer):
 
     
 class ConversationSerializer(serializers.ModelSerializer):
-    users = UserSerializer(many=True, read_only=True)
-    last_message = serializers.SerializerMethodField('get_last_messages')
-    title = serializers.SerializerMethodField('get_conversation')
-    # last_message = serializers.SerializerMethodField('get_last_message')
+    last_message = serializers.SerializerMethodField('get_last_message')
+    title = serializers.SerializerMethodField('get_conversation_title')
+
     
     class Meta:
         model = Conversation
-        fields = ['id', 'users', 'status', 'conversation_name', 'created_at', 'last_message','title']
+        fields = ['id', 'title','last_message','created_at']
     
-    def get_last_messages(self, obj):
-        # print('before')
+    def get_last_message(self, obj):
         conversation = Conversation.objects.filter(conversation_name=obj.conversation_name).first()
-        # print('after')
         messages = Message.objects.filter(conversation=conversation.id)
         messages_serializer = MessageSerializer(messages, many=True)
         return (messages_serializer.data[-1])
 
-        
-        
-    # def get_last_message(self, obj):
-        
-    #     conversation = Conversation.objects.filter(Conversation, conversation_name=obj.conversation_name)
-    #     message = Message.objects.filter(conversation=conversation.id).last()
-    #     if message:
-    #         message_serializer = MessageSerializer(message)
-    #         return message_serializer.data
-    #     else:
-    #         return {'Error': 'No message found'}
-        
-
-    
-    
-    def get_conversation(self, obj):
+    def get_conversation_title(self, obj):
         name = obj.conversation_name.split('_')
         if name[0] == "chat":
             user = obj.users.first()
