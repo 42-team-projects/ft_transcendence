644c89ef8a581850c619986c0a83a6ab571d2345
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-08-28 18:32:15 +0100

    get user data

--- backend/Profile/admin.py
+++ backend/Profile/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import User, User_Profile, Links, Stats, Graph, Achievements
+from .models import  User_Profile, Links, Stats, Graph, Achievements
 
 # Register your models here.
 
-admin.site.register(User)
 admin.site.register(User_Profile)
 admin.site.register(Links)
 admin.site.register(Stats)
