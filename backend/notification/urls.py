d3b5bc5484f91d5add40d07e54359703dbd84d96
m-zeroual <mohammedzeroual200@gmail.com>
2024-08-29 19:11:06 +0100

    chat and notification is done.

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -3,8 +3,8 @@ from . import views as notification_views
 
 
 urlpatterns = [
-    path('receive', notification_views.receive, name='notifcation-view'),
-    path('send', notification_views.send, name='notifcation-view'),
+    path('receive/', notification_views.receive, name='notifcation-view'),
+    path('send/', notification_views.send, name='notifcation-view'),
     # path('2/', notification_views.notification_index2, name='notifcation-view2'),
     # path('3/', notification_views.notification_index3, name='notifcation-view3')
 ]
