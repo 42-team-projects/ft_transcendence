7e8a0ec8774a6391c487e376739c28e957d93ba2
ogorfti <ww.shanks1@gmail.com>
2024-08-30 18:05:43 +0100

    add email confirmation cpn

--- frontend/User/EmailConf.js
+++ frontend/User/EmailConf.js
@@ -1,17 +1,41 @@
 
 export default class EmailConf extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-            <h1>Email Confirmation</h1>
-            <p>We have sent an email to your inbox. Please check it to confirm your registration.</p>
-        `;
+    async connectedCallback() {
+        const { router } = await import('../root/Router.js');
+        const params = new URLSearchParams(window.location.search);
+        const token = params.get('token');
+
+        if (token)
+        {
+            fetch(`http://127.0.0.1:8000/api/v1/auth/confirm-email/${token}`)
+            .then(response => response.json())
+            .then(data => {
+                router.handleRoute('/login')
+
+                const messageDiv = document.querySelector('#error-message');
+                messageDiv.innerHTML = data.message;
+
+                if (data.status === 'success') {
+                    messageDiv.classList.add('show', 'success');
+                } else if (data.status === 'error') {
+                    messageDiv.classList.add('show', 'error');
+                }
+            });
+        }
+        else
+        {
+            this.innerHTML = /*html*/`
+                <link rel="stylesheet" href="../user.css" type="text/css">
+                
+                <header-cpn></header-cpn>
+
+                <div id="center-container">
+                    <img src="../svg/confirm.svg" id="confirm" alt="confirm">
+                    <h1 id="confirm-header">Email Confirmation</h1>
+                    <p>We sent you a verification email to your email account<br>
+                    please check your email box.</p>
+                </div>
+            `;
+        }
     }
 }
-
-// export class HomePage extends HTMLElement {
-//     connectedCallback() {
-//         this.innerHTML = `
-//             <h1>Welcome to the Home Page</h1>
-//         `;
-//     }
-// }
