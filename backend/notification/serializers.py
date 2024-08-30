b0363d22316afc43a535863dadfc547d7c9ab738
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-08-30 10:16:17 +0100

    notification is done.

--- backend/notification/serializers.py
+++ backend/notification/serializers.py
@@ -1,8 +1,7 @@
-
 from rest_framework import serializers
 from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'user', 'is_read', 'content', 'create_at']
+        fields = ['id', 'user', 'content', 'create_at']
--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -5,6 +5,7 @@ from . import views as notification_views
 urlpatterns = [
     path('receive/', notification_views.receive, name='notifcation-view'),
     path('send/', notification_views.send, name='notifcation-view'),
+    path('notifications_list/', notification_views.list_user_notifications, name='notifcation-view'),
     # path('2/', notification_views.notification_index2, name='notifcation-view2'),
     # path('3/', notification_views.notification_index3, name='notifcation-view3')
 ]
