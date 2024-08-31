3d4f0eba690b547db215fc122984f57c197065a3
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-08-31 20:56:34 +0100

    link user account with player model.

--- backend/Player/Serializers/PlayerSerializer.py
+++ backend/Player/Serializers/PlayerSerializer.py
@@ -6,19 +6,10 @@ from .AchievementsSerializer import AchievementsSerializer
 from accounts.serializers import UserRegisterSerializer
 
 class PlayerSerializer(serializers.ModelSerializer):
-    # player = UserRegisterSerializer()
+    user = UserRegisterSerializer()
     links = LinksSerializer(many=True)
     stats = StatsSerializer()
     achievements = AchievementsSerializer(many=True)
     class Meta:
         model = Player
-        fields = ['id', 'fullName', 'profile_picture', 'cover', 'joinDate', 'active', 'stats', 'achievements', 'links']
-
-    # def update(self, instance, validated_data):
-    #     user_data = validated_data.pop('user', None)
-    #     if user_data:
-    #         user_serializer = LoginSerializer(instance.user, data=user_data, partial=True)
-    #         user_serializer.is_valid(raise_exception=True)
-    #         user_serializer.save()
-        
-    #     return super().update(instance, validated_data)
+        fields = ['id', 'user', 'fullName', 'profile_picture', 'cover', 'joinDate', 'active', 'stats', 'achievements', 'links']
