0276a39843ae4c7147b567d19c12c9b2a7ab1641
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-09-01 19:05:10 +0100

    routing

--- backend/game/views.py
+++ backend/game/views.py
@@ -7,15 +7,31 @@ from rest_framework.response import Response
 # Create your views here.
 from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
+from rest_framework.permissions import IsAuthenticated
 
-# class ListPlayer(generics.ListAPIView):
-#     queryset = Player.objects.all()
-#     serializer_class = PlayerSerializer
 
-# class DetailsPlayer(generics.RetrieveAPIView):
-#     queryset = Player.objects.all()
-#     serializer_class = PlayerSerializer
+# i want to list just the player that authenticated wih the incoming token
 
+
+class ListPlayer(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    
+class RetrievePlayer(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PlayerSerializer
+
+    def get_object(self):
+        return Player.objects.get(user=self.request.user)
+
+#i want details player to be check by id
+
+class DetailsPlayer(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    
 # @api_view(['POST'])
 # def login(request):
 #     # return id of player and username
@@ -30,17 +46,17 @@ from django.http import HttpResponse, JsonResponse
 
 
 # @csrf_exempt
-@api_view(['POST'])
-def login(request):
-    # printRequest(request)
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        username = data.get('username')
-        password = data.get('password')
-        try:
-            player = Player.objects.get(username=username, password=password)
-            serializer = PlayerSerializer(player)
-            return JsonResponse({'status': 'success', 'player': serializer.data}, status=200)
-        except Player.DoesNotExist:
-            return JsonResponse({'status': 'error', 'message': 'Invalid email or password'}, status=400)
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
+# @api_view(['POST'])
+# def login(request):
+#     # printRequest(request)
+#     if request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         username = data.get('username')
+#         password = data.get('password')
+#         try:
+#             player = Player.objects.get(username=username, password=password)
+#             serializer = PlayerSerializer(player)
+#             return JsonResponse({'status': 'success', 'player': serializer.data}, status=200)
+#         except Player.DoesNotExist:
+#             return JsonResponse({'status': 'error', 'message': 'Invalid email or password'}, status=400)
+#     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
