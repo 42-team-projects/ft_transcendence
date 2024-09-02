fbdad9b017f7f0dac3c0938709fefb742cf84613
ogorfti <ww.shanks1@gmail.com>
2024-09-02 17:07:20 +0100

    add logout api endpoint and fix routing by adding accessToken validation

--- frontend/Components/User/Header.js
+++ frontend/Components/User/Header.js
@@ -1,7 +1,7 @@
 export default class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <link rel="stylesheet" href="./style.css">
+            <link rel="stylesheet" href="Components/User/style.css">
             <div class="header">
                 <h1>FT_TRANSCENDENCE</h1>
                 <img src="../../assets/auth-svg/separator.svg">
