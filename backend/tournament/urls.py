b04a7ab2545b461497af707d9ecb4fad9ef3dab0
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-09-03 17:34:41 +0100

    updating tournament api.

--- backend/tournament/urls.py
+++ backend/tournament/urls.py
@@ -4,18 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("list_tournaments/", views.list_tournaments, name="list_tournaments"),
-    path('tournament/<int:tournamentId>/player/<int:playerId>/', views.player_join_tournament, name='player_join_tournament'),
+    path('tournament/<int:tournamentId>/player/', views.player_join_tournament, name='player_join_tournament'),
     path('SetStartDate/', views.SetStartDate, name='SetStartDate'),
     # path('tournament/<int:tournamentId>/', views.get_tournament_by_id, name='get_tournament_by_id'),
 
 
-    path('player/<int:player_id>/', views.get_tournaments_by_player_id, name='get_tournaments_by_player_id'),
+    path('player/', views.get_tournaments_by_player_id, name='get_tournaments_by_player_id'),
     path('available_tournaments/', views.get_available_tournaments, name='get_available_tournaments'),
-    path('create/player/<int:player_id>/', views.create_tournament, name='create_tournament'),
-    path('tournament/<int:tournamentId>/player/<int:playerId>/leave/', views.player_leave_tournament, name='player_leave_tournament'),
-    path('<int:id>/', views.get_tournament_by_id, name='get_tournament_by_id'),
-
-
+    path('create/player/', views.create_tournament, name='create_tournament'),
+    # path('create/player/<int:player_id>/', views.create_tournament, name='create_tournament'),
+    path('tournament/<int:tournamentId>/player/leave/', views.player_leave_tournament, name='player_leave_tournament'),
+    path('<int:tournamentId>/', views.get_tournament_by_id, name='get_tournament_by_id'),
 
 ]
 
