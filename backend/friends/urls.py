a1f0ee44e96c7e33e29f46ea82a058be7f4f7dcb
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-09-07 15:05:59 +0100

    add unfriend_user view to handle delete user from friendship.

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('send_friend_request/', friend_views.send_friend_request, name='send-friend-request'),
     path('friend_requests/', friend_views.friend_requests, name='friend-requests'),
     path('accept_friend_request/', friend_views.accept_friend_request, name='accept-friend-request'),
+
+    path('remove_friend/', friend_views.remove_friend, name='remove-friend'),
 ]
