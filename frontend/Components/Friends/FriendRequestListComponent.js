45d814d3068e761f8c902f7bc77790d8db8ce2ba
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-09-09 13:02:46 +0100

    create a friends request list in right side bar window.

--- frontend/Components/Friends/FriendRequestListComponent.js
+++ frontend/Components/Friends/FriendRequestListComponent.js
@@ -1,54 +1,76 @@
+import { getCurrentUserId } from "/Utils/GlobalVariables.js";
+import { NotificationComponent } from "/Components/Notification/NotificationComponent.js";
+import { displayNotification } from "/Components/Notification/NotificationUtils.js";
+import { MessageNotification } from "/Components/Notification/templates/MessageNotification.js";
+import { getApiData } from "/Utils/APIManager.js";
+import { NOTIFICATIONS_API_URL } from "/Utils/APIUrls.js";
+import { createNotification } from "/Components/Notification/configs/NotificationManager.js";
+import { hideFriendsRequestList } from "/Components/Header/header-bar.js";
+
+
 export class FriendRequestListComponent extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode : 'open'})
-        this.shadowRoot.innerHTML = `
-            <style> ${cssContent} </style>
-            <div class="icon">
-                <img loading="lazy" draggable="false" src="/assets/icons/account-icon.svg" alt="friends">
+        this.innerHTML = `
+            <div class="notificationsBar-header">
+                <img class="close-button" src="/assets/icons/close-icon.svg" width="24px"/>
+                <h3>Friends Requests</h3>
             </div>
-            <div class="mainContainer">
-
+            <div class="notificationsBar-body">
+                <div class="notification-list"></div>
+            </div>
+            <div class="notificationsBar-footer">
+                <h4 class="clear-all">Accept All</h4>
+                <h4 class="clear-all">Reject All</h4>
             </div>
         `;
     }
 
-    connectedCallback() {
+    async connectedCallback() {
+        this.style.width = this.width;
+        this.style.height = this.height;
+        const notificationList = this.querySelector(".notificationsBar-body");
 
+        // const notifications = await getApiData(NOTIFICATIONS_API_URL + "notifications_list/");
+        // if (notifications) {
+        //     Array.from(notifications).forEach( notif => {
+        //         const notification = createNotification(notif.user.username, notif.content, "message");
+        //         this.appendNotification(notification);
+        //     });
+        // }
+        
+        this.querySelector(".close-button").addEventListener("click", () => {
+            hideFriendsRequestList();
+        });
+        this.querySelector(".clear-all").addEventListener("click", () => {
+            this.querySelector(".notification-list").innerHTML = '';
+        });
+        notificationList.scrollTop = notificationList.scrollHeight;
     }
+    disconnectedCallback() {
+    }
+
+    appendFriendRequest(notificationContent) {
+        const notification = new NotificationComponent();
+        notification.width = "100%";
+        notification.appendChild(notificationContent);
+        this.querySelector(".notification-list").prepend(notification);
+    }
+
+    static observedAttributes = ["width", "height"];
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+        if (attrName == "width")
+            this.style.width = newValue;
+        else if (attrName == "height")
+            this.style.height = newValue;
+    }
+
+    set width(val) {this.setAttribute("width", val);}
+    get width() { return this.getAttribute("width"); }
+
+    set height(val) {this.setAttribute("height", val);}
+    get height() { return this.getAttribute("height"); }
 }
 
-customElements.define("friend-request-list", FriendRequestListComponent);
-
-const cssContent = /*css*/`
-
-    * {
-        margin: 0;
-        padding: 0;
-    }
-
-
-    :host {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    .icon {
-        height: 100px;
-        height: 100px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .mainContainer {
-        position: absolute;
-        bottom: 0;
-        width: 100px;
-        height: 100px;
-    }
-
-
-`;
+customElements.define("friends-request-list", FriendRequestListComponent);
