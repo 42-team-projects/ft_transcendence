2037a81284825ee6bb84c2824ad91736c610f000
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-09-09 12:37:07 +0100

    add sender and recever in notification message, and add NotificationViewSerializer to serialize data for View.

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -6,9 +6,13 @@ from . import views as friend_views
 
 
 urlpatterns = [
-    path('list_friend_requests/', friend_views.get_friend_requests, name='list-friend-requests'),
-    path('send_friend_request/', friend_views.send_friend_request, name='send-friend-request'),
-    path('accept_friend_request/', friend_views.accept_friend_request, name='accept-friend-request'),
+    path('friend_list/', friend_views.get_friend_list, name='friend-list'),
+    path('request_list/', friend_views.get_friend_requests, name='request-list'),
+    path('send/', friend_views.send_friend_request, name='send-friend-request'),
+    path('accept/', friend_views.accept_friend_request, name='accept-friend-request'),
+    path('unfriend/', friend_views.unfriend_user, name='unfriend-user'),
 
-    path('unfriend_user/', friend_views.unfriend_user, name='unfriend-user'),
+
+    path('block/', friend_views.block_user, name='block-user'),
+    path('unblock/', friend_views.unblock_user, name='unblock-user'),
 ]
