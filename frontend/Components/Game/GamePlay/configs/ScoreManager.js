88661a2b2f3ce6a2f122f18e8cd8e38fd409cdcb
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2024-09-10 16:31:25 +0100

    update pause page

--- frontend/Components/Game/GamePlay/configs/ScoreManager.js
+++ frontend/Components/Game/GamePlay/configs/ScoreManager.js
@@ -4,12 +4,12 @@ export function goNextStage(playerState, tournament_id) {
         // call endpoint of erase player
         // closeAndRemovePlayerFromTournament(data);
         console.log("you are lose !!!!: ", tournament_id);
-        alert("you are lose !!!!: " + tournament_id);
+        // alert("you are lose !!!!: " + tournament_id);
         
     }
     else {
         console.log("you are win !!!!: ", tournament_id);
-        alert("you are win !!!!: " + tournament_id);
+        // alert("you are win !!!!: " + tournament_id);
         // const data = gettournamentnyId(tournament_id);
         // CheckTournamentStages(data)
     }
