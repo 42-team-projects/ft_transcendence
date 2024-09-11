224a486590ee39e8f47b15ee1c55f09b58199ce9
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-09-11 19:53:34 +0100

    cleaning my code, get IDs in url.

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -8,11 +8,11 @@ from . import views as friend_views
 urlpatterns = [
     path('friends/', friend_views.get_friends_list, name='friends-list'),
     path('requests/', friend_views.get_friend_requests, name='requests-list'),
-    path('send/', friend_views.send_friend_request, name='send-friend-request'),
-    path('accept/', friend_views.accept_friend_request, name='accept-friend-request'),
-    path('unfriend/', friend_views.unfriend_user, name='unfriend-user'),
-    path('decline/', friend_views.decline_friend_request, name='decline-friend-request'),
-    path('cancel/', friend_views.cancel_friend_request, name='cancel-friend-request'),
+    path('send/<int:receiver_id>/', friend_views.send_friend_request, name='send-friend-request'),
+    path('accept/<int:request_id>/', friend_views.accept_friend_request, name='accept-friend-request'),
+    path('unfriend/<int:friend_id>/', friend_views.unfriend_user, name='unfriend-user'),
+    path('decline/<int:request_id>/', friend_views.decline_friend_request, name='decline-friend-request'),
+    path('cancel/<int:request_id>/', friend_views.cancel_friend_request, name='cancel-friend-request'),
 
     # block user
 
