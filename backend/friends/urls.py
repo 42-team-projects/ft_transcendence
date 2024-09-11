3eb965ad4c17e53749ac363fc08dfb1b1f11b767
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-09-11 10:21:23 +0100

    add block and unblock friend endpoint to view file, with there model BlockUser

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     path('send/', friend_views.send_friend_request, name='send-friend-request'),
     path('accept/', friend_views.accept_friend_request, name='accept-friend-request'),
     path('unfriend/', friend_views.unfriend_user, name='unfriend-user'),
-
     path('decline/', friend_views.decline_friend_request, name='decline-friend-request'),
     path('cancel/', friend_views.cancel_friend_request, name='cancel-friend-request'),
 
+    # block user
 
-    # path('block/', friend_views.block_user, name='block-user'),
-    # path('unblock/', friend_views.unblock_user, name='unblock-user'),
+    path('block/', friend_views.block_user, name='block-user'),
+    path('unblock/', friend_views.unblock_user, name='unblock-user'),
 ]
