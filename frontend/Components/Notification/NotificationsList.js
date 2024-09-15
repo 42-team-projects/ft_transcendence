d5397f5bd91ddc256ea90292f9cb77ec99c445b6
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-09-15 12:52:56 +0100

    fix the notification problem

--- frontend/Components/Notification/NotificationsList.js
+++ frontend/Components/Notification/NotificationsList.js
@@ -38,7 +38,7 @@ export class NotificationsList extends HTMLElement {
             notificationList.innerHTML = "";
 
             Array.from(notifications).forEach( notif => {
-                const notification = createNotification(notif.id, notif.sender.username, notif.content, notif.type);
+                const notification = createNotification(notif.id, notif.sender.username, notif.content, notif.type, notif.data);
                 this.appendNotification(notification);
             });
             // increment the notification counter.
