84d57498b41197167bd6f4cd1f704beb831ab6b4
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-09-17 21:31:04 +0100

    add nickname model to backend.

--- backend/Player/Models/StatsModel.py
+++ backend/Player/Models/StatsModel.py
@@ -10,29 +10,29 @@ class Stats(models.Model):
     xp = models.IntegerField(default=0)
     graph = models.OneToOneField(Graph, on_delete=models.CASCADE, related_name='graph', null=True, blank=True)
 
-    LEAGUES = {
-        "bronze": 1000,
-        "silver": 2000,
-        "gold": 3000,
-        "platinum": 4000,
-        "legendary": 6000
-    }
+    # LEAGUES = {
+    #     "bronze": 1000,
+    #     "silver": 2000,
+    #     "gold": 3000,
+    #     "platinum": 4000,
+    #     "legendary": 6000
+    # }
 
     def __str__(self):
         return self.league
     
-    def save(self, *args, **kwargs):
-        # Automatically calculate xp as win - loss multiplied by 150
-        self.xp = (self.win - self.loss) * 150
+    # def save(self, *args, **kwargs):
+    #     # Automatically calculate xp as win - loss multiplied by 150
+    #     self.xp = (self.win - self.loss) * 150
         
-        # Rank calculation based on xp (example: 1 rank per 1000 xp)
-        self.rank = self.xp // 1000
+    #     # Rank calculation based on xp (example: 1 rank per 1000 xp)
+    #     self.rank = self.xp // 1000
 
-        # Update progress_bar based on current xp and next league threshold
-        for league, xp_threshold in self.LEAGUES.items():
-            if self.xp < xp_threshold:
-                self.league = league
-                self.progress_bar = (self.xp * 100) // xp_threshold  # percentage progress
-                break
+    #     # Update progress_bar based on current xp and next league threshold
+    #     for league, xp_threshold in self.LEAGUES.items():
+    #         if self.xp < xp_threshold:
+    #             self.league = league
+    #             self.progress_bar = (self.xp * 100) // xp_threshold  # percentage progress
+    #             break
 
-        super().save(*args, **kwargs)
+    #     super().save(*args, **kwargs)
