27db8157e50b70ec1b85bbb96dcbc833374d6fc6
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-09-19 10:48:18 +0100

    my last commit.

--- frontend/Utils/TournamentManager.js
+++ frontend/Utils/TournamentManager.js
@@ -1,11 +1,8 @@
 import { createTournamentWebSocket, update_start_date } from "./TournamentWebSocketManager.js";
 
 export async function checkIsTournamentFull(data, tournamentWebSocket) {
-    console.log("data.number_of_players:    ", data.number_of_players);
-    console.log("data.players.length:    ", data.players.length);
     let t = 86400;
     if (data.number_of_players == data.players.length) {
-        console.log("\nhere\n");
         t = 0;
         tournamentWebSocket.send(JSON.stringify({'type': 'play_cancel', 'message': 'Tournament is starting in 2 minutes'}));
     }
