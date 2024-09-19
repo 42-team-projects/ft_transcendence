6622a3da2cd6503ccc37b65fc5dc2ad96e89b721
Mohammed Zeroual <mohammedzeroual200@gmail.com>
2024-09-19 16:27:20 +0100

    fix error in friend application (can't send message because not friends).

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -3,6 +3,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    # re_path(r'ws/chat/group/(?P<room_name>\w+)/$', consumers.GroupConversationConsumer.as_asgi())
+    re_path(r'ws/chat/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi())
 ]
